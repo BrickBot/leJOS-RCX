5b86eb58d272f7b52cb2706d375a2d2eea75bd22
jose <jose@4432cd44-f382-4be9-9dff-e810c426dc2b>
2000-01-28 05:09:58 +0000

    Rearranged fields and removed static initializer

--- trunk/lejos/src/java/classes/java/lang/Thread.java
+++ trunk/lejos/src/java/classes/java/lang/Thread.java
@@ -2,18 +2,10 @@ package java.lang;
 
 public abstract class Thread
 {
-  static
-  {
-    new Thread ("main") {
-      public void run() {
-        Thread._TVM_mainMethod();
-      }
-    }.start();
-  }    
-
   // Note: The following fiels are used by the VM.
   // Their sizes and location can only be changed
-  // if classes.h is changed accordingly.
+  // if classes.h is changed accordingly. Needless
+  // to say, they are read-only.
 
   private Thread _TVM_nextThread;
   private Object _TVM_waitingOn;
@@ -30,19 +22,18 @@ public abstract class Thread
   // Static state follows:
 
   private static byte _TVM_threadIdCounter;
-  private static final OutOfMemoryError _TVM_outOfMemoryError =
-    new OutOfMemoryError();
+  private static _TVM_outOfMemoryError = new OutOfMemoryError();
   
   public Thread()
   {
-    this ("n/a");
+    this ("");
   }
 
   public Thread (String name)
   {
     this.name = name;
     if (_TVM_threadIdCounter >= 63)
-      throw _TVM_outOfMemoryError;
+      _TVM_throwOutOfMemoryError;
     _TVM_threadId = ++_TVM_threadIdCounter;
   }
   
@@ -53,7 +44,6 @@ public abstract class Thread
 
   public native void start();
   public abstract void run();
-  private static native void _TVM_mainMethod();
 }
 
 
