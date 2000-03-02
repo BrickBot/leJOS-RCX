bceb1a2431ab0b599b78d9c5fd4882007b23749f
jose <jose@4432cd44-f382-4be9-9dff-e810c426dc2b>
2000-03-02 03:27:03 +0000

    Putting back before changing array access stuff.

--- test/regression/Test05.java
+++ test/regression/Test05.java
@@ -9,8 +9,8 @@ public class Test05
   {
     int[] pTones = new int[] { 1000, 1000, 1400, 1800, 1000, 600, 
 			       1400, 1000, 200 };
-    short[] pDurations = new short[] { 100, 50, 50, 100, 100, 100, 
-			       50, 50, 100 };
+    short[] pDurations = new short[] { 50, 25, 25, 50, 50, 50, 
+			       25, 25, 200 };
 
     for (int i = 0; i < pTones.length; i++)
     {
