037679f55d1f66907f5f3d303d66acf2c34995ea
jose <jose@4432cd44-f382-4be9-9dff-e810c426dc2b>
2000-03-05 12:01:53 +0000

    *** empty log message ***

--- trunk/lejos/src/java/classes/java/lang/Object.java
+++ trunk/lejos/src/java/classes/java/lang/Object.java
@@ -2,6 +2,17 @@ package java.lang;
 
 public class Object
 {
+  /**
+   * Returns <code>null</code>. It's here to satisfy javac.
+   */
+  public Class getClass()
+  {
+    return null;
+  }
+
+  /**
+   * Returns the empty string. It's here to satisfy javac.
+   */
   public String toString()
   {
     return "";
