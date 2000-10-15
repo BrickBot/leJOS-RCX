f55d7beb67a8913cf05230cc21f4e0a90e4b5d98
jhsolorz <jhsolorz@4432cd44-f382-4be9-9dff-e810c426dc2b>
2000-10-15 23:42:04 +0000

    file is generated

--- src/platform/unix/nativeemul.c
+++ src/platform/unix/nativeemul.c
@@ -8,6 +8,7 @@
 #include "constants.h"
 #include "specialsignatures.h"
 #include "specialclasses.h"
+#include "stack.h"
 #include "memory.h"
 #include "threads.h"
 #include "classes.h"
@@ -19,6 +20,9 @@
 
 static TWOBYTES gSensorValue = 0;
 
+/**
+ * NOTE: The technique is not the same as that used in TinyVM.
+ */
 void dispatch_native (TWOBYTES signature, STACKWORD *paramBase)
 {
   ClassRecord *classRecord;
@@ -26,45 +30,38 @@ void dispatch_native (TWOBYTES signature, STACKWORD *paramBase)
   switch (signature)
   {
     case START_V:
-      if (!init_thread ((Thread *) word2ptr(paramBase[0])))
-	return;
-      break;
+      init_thread ((Thread *) word2ptr(paramBase[0]));
+      return;
     case YIELD_V:
-      // Pop current stack frame
-      do_return (0);
-      // Switch current thread
       switch_thread();
-      // Go back and continue
       return;
     case SLEEP_V:
       sleep_thread (paramBase[1]);
-      do_return (0);
       switch_thread();
       return;
     case CURRENTTIMEMILLIS_J:
-      *(++stackTop) = 0;
-      *(++stackTop) = get_sys_time();
-      do_return (2);
+      push_word (0);
+      push_word (get_sys_time());
       return;
     case CALLROM0_V:
       printf ("& ROM call 0: 0x%lX\n", paramBase[0]);
-      break;      
+      return;      
     case CALLROM1_V:
       printf ("& ROM call 1: 0x%lX (%ld)\n", paramBase[0], paramBase[1]);
-      break;      
+      return;      
     case CALLROM2_V:
       printf ("& ROM call 2: 0x%lX (%ld, %ld)\n", paramBase[0],
                                                   paramBase[1],
                                                   paramBase[2]
              );
-      break;      
+      return;      
     case CALLROM3_V:
       printf ("& ROM call 3: 0x%lX (%ld, %ld, %ld)\n", paramBase[0],
                                                      paramBase[1],
                                                      paramBase[2],
                                                      paramBase[3]
              );
-      break;      
+      return;      
     case CALLROM4_V:
       printf ("& ROM call 4: 0x%lX (%ld, %ld, %ld, %ld)\n", paramBase[0],
                                                      paramBase[1],
@@ -72,35 +69,32 @@ void dispatch_native (TWOBYTES signature, STACKWORD *paramBase)
                                                      paramBase[3],
                                                      paramBase[4]
              );
-      break;      
+      return;      
     case READMEMORYBYTE_B:
       printf ("& Attempt to read byte from 0x%lX\n", (paramBase[0] & 0xFFFF));
       if ((paramBase[0] & 0x0001) == 0)
-        *(++stackTop) = gSensorValue >> 8;
+        push_word (gSensorValue >> 8);
       else
       {
-        *(++stackTop) = gSensorValue & 0xFF;
+        push_word (gSensorValue & 0xFF);
         gSensorValue++;
       }
-      do_return (1);
       return;
     case WRITEMEMORYBYTE_V:
       printf ("& Attempt to write byte [%lX] at 0x%lX (no effect)\n", paramBase[1] & 0xFF, paramBase[0] & 0xFFFF);
-      break;
+      return;
     case SETMEMORYBIT_V:
-       printf ("& Attempt to set memory bit [%ld] at 0x%lX (no effect)\n", paramBase[1] & 0xFF, paramBase[0] & 0xFFFF);
-      break;      
+      printf ("& Attempt to set memory bit [%ld] at 0x%lX (no effect)\n", paramBase[1] & 0xFF, paramBase[0] & 0xFFFF);
+      return;      
     case GETDATAADDRESS_I:
-      *(++stackTop) = ptr2word (((byte *) word2ptr (paramBase[0])) + HEADER_SIZE);
-      do_return (1);
+      push_word (ptr2word (((byte *) word2ptr (paramBase[0])) + HEADER_SIZE));
       return;
     case RESETSERIAL_V:
       printf ("& Call to resetRcx");
-      break;
+      return;
     default:
       throw_exception (noSuchMethodError);
   }
-  do_return (0);
 } 
 
 
