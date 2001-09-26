c2d5334241505d0b133a5eb72bd4c6e8644421b1
mpandrews <mpandrews@4432cd44-f382-4be9-9dff-e810c426dc2b>
2001-09-26 02:31:10 +0000

    Added ProximitySensor and ListenerThread

---
+++ src/java/classes/josx/platform/rcx/ListenerThread.java
@@ -0,0 +1,63 @@
+package josx.platform.rcx;
+
+class ListenerThread extends Thread
+{
+  static ListenerThread singleton = new ListenerThread();
+  
+  int mask;
+  Poll poller = new Poll();   
+
+  static ListenerThread get()
+  {
+      synchronized (singleton)
+      {
+          if (!singleton.isAlive())
+          {
+            singleton.setDaemon(true);
+            singleton.setPriority(Thread.MAX_PRIORITY);
+            singleton.start();
+          }
+      }
+      
+      return singleton;
+  }
+
+  void addToMask(int mask)
+  {
+     this.mask |= mask;
+      
+      // Interrupt the polling thread, not the current one!
+     interrupt();
+  }
+
+  void addButtonToMask(int id) {
+      addToMask(id << Poll.BUTTON_MASK_SHIFT);
+  }
+  
+  void addSensorToMask(int id) {
+      addToMask(1 << id);
+  }
+  
+  public void run()
+  {
+      for (;;) {
+          try  {
+              int changed = poller.poll(mask, 0);
+              
+              if ((changed & Poll.SENSOR1_MASK) != 0)
+                  Sensor.S1.callListeners();
+              if ((changed & Poll.SENSOR2_MASK) != 0)
+                  Sensor.S2.callListeners();
+              if ((changed & Poll.SENSOR3_MASK) != 0)
+                  Sensor.S3.callListeners();
+              if ((changed & Poll.RUN_MASK) != 0)
+                  Button.RUN.callListeners();
+              if ((changed & Poll.VIEW_MASK) != 0)
+                  Button.VIEW.callListeners();
+              if ((changed & Poll.PRGM_MASK) != 0)
+                  Button.PRGM.callListeners();
+          } catch (InterruptedException ie) {
+          }
+      }
+  }
+}
