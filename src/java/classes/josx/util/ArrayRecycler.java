a1b1aecb828e4af718b3cf1cc2eed7e15422ccf9
jhsolorz <jhsolorz@4432cd44-f382-4be9-9dff-e810c426dc2b>
2001-11-18 15:01:57 +0000

    Recyler now uses list. Added ArrayRecycler.

---
+++ src/java/classes/josx/util/ArrayRecycler.java
@@ -0,0 +1,45 @@
+package josx.util;
+
+/**
+ * An abstract array recycler. 
+ * <p>
+ * Note that the caller is expected to provide
+ * thread safety for instances of this class.
+ * 
+ * @see josx.util.RecyclableArray.
+ */
+public final class ArrayRecycler extends Recycler {
+	private int requestedLength;
+	
+    /**
+     * Constructs a recycler.
+     * @param capacity Maximum number of allocated (non garbage) objects at any given time.
+     */
+    public ArrayRecycler() {
+    }
+    
+    /**
+     * Attempts to obtain a free RecyclableArray.
+     * @return A RecyclableArray reference.
+     * @throws java.lang.StackOverflowError May be thrown due to the recursive implementation of the method.
+     */
+    public final RecyclableArray allocate (int length) {
+		RecyclableArray array1;
+		this.requestedLength = length;
+        array1 = (RecyclableArray) allocate();
+		if (array1.getCapacity() >= length) {
+			array1.init (length);
+			return array1;
+		}
+		try {
+		   return allocate (length);
+		} finally {
+		   // Must not recycle before calling allocate (length).
+		   recycle (array1);
+		}
+	}	    
+
+	protected final Recyclable createInstance() {
+		return new RecyclableArray (this.requestedLength);
+	}
+}
--- src/java/classes/josx/util/Recyclable.java
+++ src/java/classes/josx/util/Recyclable.java
@@ -11,17 +11,22 @@ public interface Recyclable {
 	public void init();
 	
 	/**
-	 * Called by users when this Recyclable is no longer needed.
+	 * Called when this Recyclable is no longer needed.
 	 * Resources should be disposed in this method, e.g. this is where
 	 * any nested Recyclables would be released.
 	 */
 	public void release();
-	
+
 	/**
-	 * Determines whether this Recyclable is garbage or not.
-	 * It should return true before the first time init() is
-	 * invoked. It should return false after init() is invoked.
-	 * It should return false after release() is invoked.
+	 * Must return Recyclable most recently set with
+	 * <code>setNextRecyclable</code>.
+	 * @see josx.util.Recyclable#setNextRecyclable
 	 */
-	public boolean isGarbage();
+    public Recyclable getNextRecyclable();
+
+	/**
+	 * Stores a Recyclable object.
+	 * @see josx.util.Recyclable#getNextRecyclable
+	 */
+    public void setNextRecyclable (Recyclable r);	
 }
