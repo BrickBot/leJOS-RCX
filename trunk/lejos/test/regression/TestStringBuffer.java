abb60692dfdf24ee5bbe43e121c5549c285ba343
mpandrews <mpandrews@4432cd44-f382-4be9-9dff-e810c426dc2b>
2001-11-30 03:01:28 +0000

    Added number formatting and fixed Math.log() and Math.exp()

---
+++ trunk/lejos/test/regression/TestStringBuffer.java
@@ -0,0 +1,71 @@
+import josx.util.Test;
+import josx.platform.rcx.*;
+
+public class TestStringBuffer
+{
+        
+    public static void main(String []args)
+    {
+        StringBuffer s1 = new StringBuffer(30);
+        s1.append("2^10=");
+        s1.append(1024);
+        s1.append("!");
+        Test.assert(s1.toString(), "2^10=1024!".equals(s1.toString()));
+
+        LCD.showNumber(1);
+                
+        s1.delete(0,30);
+        s1.append("one=");
+        s1.append(1.0);
+        s1.append("!!");
+        Test.assert(s1.toString(), "one=1.0!!".equals(s1.toString()));
+        
+        LCD.showNumber(2);
+                
+        s1.delete(0,30);
+        s1.append("pi=");
+        s1.append(3.1415927);
+        s1.append("!");
+        Test.assert(s1.toString(), "pi=3.1415927!".equals(s1.toString()));
+        
+        LCD.showNumber(3);
+                
+        s1.delete(0,30);
+        s1.append("pi!=");
+        s1.append(-3.1415927);
+        s1.append("!");
+        Test.assert(s1.toString(), "pi!=-3.1415927!".equals(s1.toString()));
+        
+        LCD.showNumber(4);
+                
+        s1.delete(0,30);
+        s1.append("pi!=");
+        s1.append(3.1415927e30);
+        s1.append("!");
+        Test.assert(s1.toString(), "pi!=3.1415927E30!".equals(s1.toString()));
+        
+        LCD.showNumber(5);
+                
+        s1.delete(0,30);
+        s1.append("pi!=");
+        s1.append(3.1415927e-30);
+        s1.append("!");
+        Test.assert(s1.toString(), "pi!=3.1415925E-30!".equals(s1.toString()));
+        
+        LCD.showNumber(6);
+                
+        s1.delete(0,30);
+        s1.append("pi!=");
+        s1.append(-3.1415927e30);
+        s1.append("!");
+        Test.assert(s1.toString(), "pi!=-3.1415927E30!".equals(s1.toString()));
+        
+        LCD.showNumber(7);
+                
+        s1.delete(0,30);
+        s1.append("pi!=");
+        s1.append(-3.1415927e-30);
+        s1.append("!");
+        Test.assert(s1.toString(), "pi!=-3.1415925E-30!".equals(s1.toString()));        
+    }
+}
