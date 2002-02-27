0c84428142d9b815db515aa3e4bc27b071aef28c
bbagnall <bbagnall@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-02-27 01:37:15 +0000

    This commit was generated by cvs2svn to compensate for changes in r262, which included commits to RCS files with non-trunk default branches.

---
+++ trunk/lejos/src/java/pcrcxcomm/josx/rcxcomm/RCXPort.java
@@ -0,0 +1,102 @@
+package josx.rcxcomm;
+
+import java.io.*;
+import josx.rcxcomm.irTower;
+
+/** A DataPort designed for use on a computer with a serial or a USB-connected
+ * IR Tower. The USB implementation only currently works for Windows.
+ * Adapted from original code made by the LEGO3 Team at DTU-IAU
+ * @author Brian Bagnall
+ * @author Lawrie Griffiths
+ */
+public class RCXPort extends DataPort {
+
+  private irTower tower;
+  int bytesRead;
+  boolean open = false;
+
+  /** Creates new RCXPort
+  * @param port. Specifies the port that the IR Tower is connected to.
+  */
+  public RCXPort(String port) throws IOException {
+    super();
+    open(port);
+  }
+
+  /** Creates new RCXPort
+   *  the port is obtained from RCXTTY, defaulting to usb
+  */
+  public RCXPort() throws IOException {
+    super();
+    open("");
+  }
+
+  private void open(String port) throws IOException {
+    tower = new irTower();
+    int r = tower.open(port);
+
+    // Throw an IO Exception if the Open fails
+    if (r < 0)
+      throw(new IOException("Open " + port + " failed with message: "
+                            + tower.strerror(-r) + ", error: "
+                            + tower.getError()));
+    else open = true;
+  }
+
+  private byte[] buffer = new byte[9];
+
+  protected boolean dataAvailable() {
+    if (bytesRead == 9) return true;
+
+    // Due the the way that DataPort and RCXPort currently interact,
+    // dataAvailable can be called before the port is open.
+    if (tower == null || !open) return false;
+
+    // Read 9 bytes into the buffer
+    bytesRead = tower.read(buffer);
+
+    return (bytesRead == 9);
+  }
+
+  private byte[] packet = {(byte)0xf7, (byte)0x00};
+
+  protected void sendByte(byte b) throws IOException {
+    packet[1] = b;
+
+    int actual = tower.send(packet,packet.length);
+    // tower.hexdump("Send",packet,packet.length);
+    if (actual != 2) {
+      throw new IOException("Failed to send whole packet");
+    }
+  }
+
+  protected byte receiveByte() throws IOException {
+    if (bytesRead == 0) throw new IOException("dataAvailable not called");
+    if (bytesRead < 9) throw new IOException("Insufficient data");
+    bytesRead = 0;
+
+    if (buffer[0]==(byte)0x55 && buffer[1]==(byte)0xff && buffer[2]==(byte)0x00)
+     if (buffer[3] == ~buffer[4] && buffer[3] == (byte)0xf7)
+        if (buffer[5] == ~buffer[6]) {
+           return buffer[5];
+        }
+    throw new IOException("Something wrong in the package.");
+  }
+
+  /** Close the RCXPort.
+  * The port can not be reopened.
+  */
+  public void close(){
+    super.close();
+    int r = tower.close();
+    open = false;
+  }
+
+  // Does nothing.
+  void setListen(boolean b) {
+  }
+}
+
+
+
+
