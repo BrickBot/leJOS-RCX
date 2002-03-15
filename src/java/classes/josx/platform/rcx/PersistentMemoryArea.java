24ea9cf3fcdaaed076dc6e11edf329296faef686
starblue <starblue@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-03-15 09:27:52 +0000

    now allocates its memory properly to coexist with heap in extra memory

--- src/java/classes/josx/platform/rcx/PersistentMemoryArea.java
+++ src/java/classes/josx/platform/rcx/PersistentMemoryArea.java
@@ -14,10 +14,22 @@ package josx.platform.rcx;
 
 public class PersistentMemoryArea
 {
-  private static final int MAGIC_ADDRESS = 0xf001;
+  // must be consistent with VM memory layout!
+  private static final int REGION_HEADER_SIZE = 4;
+  private static final int ARRAY_HEADER_SIZE  = 4;
+  private static final int IS_ALLOCATED_MASK  = 0x8000;
+  private static final int IS_ARRAY_MASK      = 0x4000;
+  private static final int ELEM_TYPE_SHIFT    = 9;
+  private static final int T_INT              = 10;
+  private static final int INT_SIZE           = 4;
+
+  private static final int REGION_ADDRESS       = 0xf002;
+  private static final int BLOCK_HEADER_ADDRESS = REGION_ADDRESS +
+                                                    REGION_HEADER_SIZE;
+  private static final int MAGIC_ADDRESS = BLOCK_HEADER_ADDRESS + 
+                                             ARRAY_HEADER_SIZE;
   private static final int SIZE_ADDRESS  = MAGIC_ADDRESS+2;
   private static final int START_ADDRESS = MAGIC_ADDRESS+4;
-  private static final int END_ADDRESS   = 0xfb80;
 
   private static PersistentMemoryArea singleton = null;
 
@@ -31,10 +43,23 @@ public class PersistentMemoryArea
     throws OutOfMemoryError  
   {
     if (singleton == null) {
-      if (START_ADDRESS + size > END_ADDRESS) {
-        throw new OutOfMemoryError();
-      } else {
-        synchronized (Memory.MONITOR) {
+      synchronized (Memory.MONITOR) {
+        short blockHeader = Memory.readShort ((short)BLOCK_HEADER_ADDRESS);
+        // magic (2) + size (2) + round up (INT_SIZE-1)
+        int arraySize = (size + 4 + INT_SIZE-1)/INT_SIZE; 
+        int blockSize = ARRAY_HEADER_SIZE + INT_SIZE * arraySize;
+
+        if ((blockHeader & IS_ALLOCATED_MASK) != 0 || // too late for us
+            blockSize > blockHeader || // not enough space left
+            arraySize > 511) {  // array would be too big
+          throw new OutOfMemoryError();
+        } else {
+          // write header for following block
+          Memory.writeShort (BLOCK_HEADER_ADDRESS+blockSize,
+                             (short)(blockHeader-blockSize));
+          // write array header
+          blockHeader = (short)(IS_ARRAY_MASK | T_INT << ELEM_TYPE_SHIFT | arraySize);
+          Memory.writeShort (BLOCK_HEADER_ADDRESS, blockHeader);
           if (Memory.readShort (MAGIC_ADDRESS) != magic
               && Memory.readShort (SIZE_ADDRESS) != size) {
             // not what we are looking for, need to reinitialize
