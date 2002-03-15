9f4e7ae6da63bd64a1b7f74c6a16d040dfa653bb
starblue <starblue@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-03-15 00:11:13 +0000

    memory regions, fix for poll hang on negative time

--- src/platform/unix/sensors.h
+++ src/platform/unix/sensors.h
@@ -10,6 +10,7 @@ typedef struct {
 } sensor_t;
 
 extern sensor_t sensors[3];
+extern void init_sensors( void);
 extern void poll_sensors( void);
 extern void read_buttons(int, short*);
 extern void check_for_data (char *valid, char **nextbyte);
