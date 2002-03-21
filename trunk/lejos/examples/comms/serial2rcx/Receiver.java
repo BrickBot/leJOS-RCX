9c03376be2fae7eb0df455990b931c2e612e2d69
bbagnall <bbagnall@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-03-21 22:06:59 +0000

    Updated with clearer example.

--- trunk/lejos/examples/comms/serial2rcx/Receiver.java
+++ trunk/lejos/examples/comms/serial2rcx/Receiver.java
@@ -1,21 +1,36 @@
 import josx.platform.rcx.*;
 
-class Receiver
-{
-	public static void main(String[] args)
-	{
-		byte[] b = new byte[8];
-		int count = 0;
-		
-		while(true)
-		{
-			if(Serial.isPacketAvailable())
-			{
-				Serial.readPacket(b);
-				Sound.beep();
-				count++;
-				LCD.showNumber(count);
-			}
-		}
-	}
+public class Receiver {
+
+   
+   public static void main(String [] args) throws Exception {
+      Receiver r = new Receiver();
+      
+      for(;;) {
+         if(Serial.isPacketAvailable()) {
+            Sound.beep();
+            LCD.showNumber(r.receiveByte());
+         }
+      }
+   }
+   
+   private byte[] packet = {(byte)0xf7, (byte)0x00};
+   
+   /**
+    * Send a single byte
+    */
+   protected void sendByte(byte b) throws Exception {
+      packet[1] = b;
+      josx.platform.rcx.Serial.sendPacket(packet, 0, 2);
+   }
+  
+   private byte[] buffer = new byte[10];
+   
+   /**
+    * Receive a single byte
+    */
+   protected byte receiveByte() throws Exception {
+      josx.platform.rcx.Serial.readPacket(buffer);
+      return buffer[1];
+   }  
 }
--- trunk/lejos/examples/comms/serial2rcx/Sender.java
+++ trunk/lejos/examples/comms/serial2rcx/Sender.java
@@ -2,15 +2,32 @@ import josx.platform.rcx.*;
 
 class Sender
 {
-	public static void main(String[] args)
-	{
-		byte[] b = new byte[] {(byte)0xf7, (byte)0x01};
-		
-		while(true)
-		{
-			for(int i = 0;  i < 1000;  i--)
-				i += 2;
-			Serial.sendPacket(b, 0, 2);
-		}
-	}
+   public static void main(String [] args) throws Exception {
+      Sender s = new Sender();
+      for(byte i=0;i<200;++i) {
+         Button.RUN.waitForPressAndRelease();
+         Sound.beep();
+         s.sendByte(i);
+      }
+   }
+   
+   private byte[] packet = {(byte)0xf7, (byte)0x00};
+   
+   /**
+    * Send a single byte
+    */
+   protected void sendByte(byte b) throws Exception {
+      packet[1] = b;
+      josx.platform.rcx.Serial.sendPacket(packet, 0, 2);
+   }
+  
+   private byte[] buffer = new byte[10];
+   
+   /**
+    * Receive a single byte
+    */
+   protected byte receiveByte() throws Exception {
+      josx.platform.rcx.Serial.readPacket(buffer);
+      return buffer[1];
+   }  
 }
