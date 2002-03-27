e6f5ddd5a649009917193286e7a6e5add4372333
lgriffiths <lgriffiths@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-03-27 22:22:23 +0000

    Initial version

---
+++ trunk/lejos/src/java/rcxcomm/java/net/Socket.java
@@ -0,0 +1,65 @@
+package java.net;
+
+import josx.rcxcomm.*;
+import java.io.*;
+
+/** An RCX version of Socket that connects to TCP/IP ports via a 
+ * proxy called SocketProxy running on the PC. It uses the 
+ * josx.rcxcomm package.
+ * @author Lawrie Griffiths
+ */
+public class Socket {
+  private RCXPort rcxport;
+  private InputStream is;
+  private OutputStream os;
+
+  /**
+   *  Connects to a Socket via the proxy on the PC.
+   * @param host the host to connect to
+   * @param port the port to connect to
+   */
+  public Socket(String host, int port) throws IOException {
+    
+    // Open the RCX Port
+
+    rcxport = new RCXPort();
+    is = rcxport.getInputStream();
+    os = rcxport.getOutputStream();
+    DataOutputStream dos = new DataOutputStream(os);
+
+    // Send an end of stream marker
+
+    dos.writeByte((byte) 0xff);
+
+    // Send the host name
+      
+    char [] hostChars = host.toCharArray();
+
+    dos.writeByte(hostChars.length);
+
+    for(int i=0;i<hostChars.length;i++) 
+      dos.writeByte((byte) hostChars[i]);
+
+    // Send the port number
+     
+    dos.writeInt(port);
+
+    // Check the reply
+
+    if (is.read() != 0) throw new IOException();
+  }
+
+  /** Returns an input stream for this Socket.
+   * @return an input stream for reading bytes from this Socket.
+   */
+  public InputStream getInputStream() {
+    return is;
+  }
+ 
+  /** Returns an output stream for this Socket.
+   * @return an output stream for writing bytes to this Socket.
+   */
+  public OutputStream getOutputStream() {
+    return os;
+  }
+}
---
+++ trunk/lejos/src/java/rcxcomm/java/net/URLConnection.java
@@ -0,0 +1,79 @@
+package java.net;
+
+import java.io.*;
+
+/** An RCX version of URLConnection that uses the RCX version of Socket. 
+ * Socket uses a proxy called SocketProxy running on the PC. It connects  
+ * to it using the josx.rcxcomm package.
+ * This version ignores all http headers.
+ * @author Lawrie Griffiths
+ */
+public class URLConnection {
+  private InputStream in;
+  private OutputStream out;
+  private Socket socket;
+
+  static final private char [] get = {'G','E','T',' '};
+  static final private char [] http = {' ','H','T','T','P','/','1','.','0','\r','\n','\r','\n'};
+
+  /**
+   * Connects to a Socket via the proxy on the PC.
+   * @param host the host to connect to
+   * @param port the port to connect to
+   * @param path the file to get 
+   */
+  public URLConnection(String host, int port, String path) throws IOException {
+
+    // Open the socket
+
+    socket = new Socket(host,port);
+    in = socket.getInputStream();
+    out = socket.getOutputStream();
+
+    // Do an http get
+
+    char [] pathChars = path.toCharArray();
+
+    for(int i=0;i<get.length;i++)
+      out.write((byte) get[i]);
+
+    for(int i=0;i<pathChars.length;i++)
+      out.write((byte) pathChars[i]);
+
+    for(int i=0;i<http.length;i++)
+      out.write((byte) http[i]);
+
+    int done = 0;
+
+    // Skip the headers
+
+    do {
+       int b = in.read();
+       if (b == '\r') {
+         if (done == 0 || done == 2) done++;
+         else done = 0;
+       } 
+       else if (b == '\n') { 
+         if (done == 1 || done == 3) done++;
+         else done = 0;
+       }
+       else done = 0;
+    } while (done < 4);
+  }
+
+  /** Returns an input stream for this URLConnection.
+   * @return an input stream for reading bytes from this URLConnection.
+   */
+  public InputStream getInputStream() {
+    return in;
+  }
+
+  /** Returns an output stream for this URLConnection.
+   * @return an output stream for writing bytes to this URLConnection.
+   */
+  public OutputStream getOutputStream() {
+    return out;
+  }
+}   
+   
+
