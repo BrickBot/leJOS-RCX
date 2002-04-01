050c64cff0a12ca83074e608d152d435a5af96ff
starblue <starblue@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-04-01 20:38:14 +0000

    corrected bug in memory allocation

--- src/platform/unix/tvmemul.c
+++ src/platform/unix/tvmemul.c
@@ -95,9 +95,10 @@ void run(void)
     TWOBYTES size;
 
     memory_init ();
+    size = EXTRA_MEMORY_SIZE;
     region = (byte *) malloc (size * sizeof (TWOBYTES));
     memory_add_region (region, region + size * sizeof (TWOBYTES));
-    size = EXTRA_MEMORY_SIZE;
+    size = MEMORY_SIZE;
     region = (byte *) malloc (size * sizeof (TWOBYTES));
     memory_add_region (region, region + size * sizeof (TWOBYTES));
   }
