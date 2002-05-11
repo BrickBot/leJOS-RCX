3abb2ca642044a18ae46d394b12ad021316677e6
lgriffiths <lgriffiths@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-05-11 14:48:40 +0000

    String parameters and documentation

--- trunk/lejos/src/java/rcxcomm/javax/servlet/http/HttpServletRequest.java
+++ trunk/lejos/src/java/rcxcomm/javax/servlet/http/HttpServletRequest.java
@@ -2,22 +2,160 @@ package javax.servlet.http;
 
 import java.io.*;
 
+/**
+ *
+ * Provides request information for HTTP servlets. 
+ *
+ * The servlet container (the abstract HttpServlet on lejos), 
+ * creates an <code>HttpServletRequest</code> 
+ * object and passes it as an argument to the servlet's service
+ * methods: (<code>doGet</code>).
+ *
+ *
+ * @author 	Lawrie Griffiths
+ *
+ */
 public class HttpServletRequest {
-  char [] path;
-  int pathLength;
-
-  public void setPath(char [] path, int pathLength) {
+  private String path;
+  private int pathLength;
+  private String queryString;
+  private int queryStringLength;
+  private static final int MAX_PARAM = 16;
+  private char [] paramChars = new char[MAX_PARAM];
+  private String param = new String(paramChars);
+  private int paramLength = 0;
+  
+  void setPath(String path, int pathLength) {
     this.path = path;
     this.pathLength = pathLength;
   }
 
-  public final char [] getServletPath() {
+  void setQueryString(String queryString, int queryStringLength) {
+    this.queryString = queryString;
+    this.queryStringLength = queryStringLength;
+  }
+
+  /**
+   * Returns the Servlet Path. 
+   *
+   * On lejos, this is the whole path up to the start of any query string    
+   * @return  the servlet path
+   */
+  public String getServletPath() {
     return path;
   }
 
-  public int getServeletPathLength() {
+  /**
+   * Returns the Request URI. 
+   *
+   * On lejos, this is the whole path up to the start of any query string.
+   * It is the same as Servlet Path.
+   *    
+   * @return  the request URI
+   */
+  public String getRequestURI() {
+    return path;
+  }
+
+  /**
+   * Returns the real Servlet Path Length. 
+   *
+   * On lejos, servlet paths are padded with spaces to re-use Strings.
+   * This extra method, returns the real length of the path.   
+   * @return  the servlet path length
+   */
+  public int getServletPathLength() {
     return pathLength;
   }
+
+  /**
+   * Returns the Query String 
+   *
+   * This is the part of a URI after any "?".
+   * It is all spaces is there is no Query String
+   *    
+   * @return  the request URI
+   */
+  public String getQueryString() {
+    return queryString;
+  }
+
+  /**
+   * Returns the real Query String Length. 
+   *
+   * On lejos, query strings are padded with spaces to re-use Strings.
+   * This extra method, returns the real length of the query string.   
+   * @return  the query string length
+   */
+  public int getQueryStringLength() {
+    return queryStringLength;
+  }
+
+  /**
+   * Returns the named parameter value, or null if there is no such parameter.
+   * The returned string is re-used, so do not rely on it after a 
+   * subsequent call of getParameter.
+   *
+   * @param name   The required parameter.
+   *    
+   * @return  the parameter value or null
+   */
+  public String getParameter(String name) {
+    char [] nameChars = name.toCharArray();
+    char [] queryStringChars = queryString.toCharArray();
+
+    // Look for paameter name. Value must be at least one character
+
+    for(int i=0;i<queryStringLength-nameChars.length-2;i++) {
+
+      // Parameter name starts at beginning or after an ampersand
+
+      if (!(i == 0 || queryStringChars[i-1] == '&')) continue;
+      int j;
+      for(j=0;j<nameChars.length;j++) {
+        if (queryStringChars[i+j] != nameChars[j]) break;
+      }
+
+      // Parameter name must be followed by an equals sign
+
+      if (j == nameChars.length && queryStringChars[j+i] == '=') {
+        // Found the parameter
+        int valueStart = i+j+1;
+
+        // Copy value to paramChars, stop at end of wuery String or at an
+        // ampersand or when length reaches MAX_PARAM
+
+        for(paramLength=0;valueStart+paramLength<queryStringLength;paramLength++) {
+          if (queryStringChars[valueStart+paramLength] == '&' ||
+              paramLength == MAX_PARAM) break;
+          paramChars[paramLength] = queryStringChars[valueStart+paramLength];
+        }
+
+        // Space fill the String
+
+        for(j=paramLength;j<MAX_PARAM;j++) paramChars[j] = ' ';
+        return param;
+      }
+    }
+
+    // Return null if the parameter is not found
+
+    return null;
+  } 
+
+  /**
+   * Returns the real length of the current parameter. 
+   *
+   * On lejos, parameters are padded with spaces to re-use Strings.
+   * This extra method, returns the real length of the parameter
+   * value returned by the latest call of getParameter().
+   *
+   * @return  the parameter value length
+   */
+  public int getParameterLength() {
+    return paramLength;
+  }
 }
+
 
 
--- trunk/lejos/src/java/rcxcomm/javax/servlet/http/HttpServletResponse.java
+++ trunk/lejos/src/java/rcxcomm/javax/servlet/http/HttpServletResponse.java
@@ -2,26 +2,53 @@ package javax.servlet.http;
 
 import java.io.*;
 
-public class HttpServletResponse {
-  OutputStream os;
- 
+/**
+ *
+ * Provides HTTP-specific functionality for sending a response. 
+ *
+ * The servlet container (the abstract HttpServer on lejos) 
+ * creates an <code>HttpServletRequest</code> object
+ * and passes it as an argument to the servlet's service method:
+ * (<code>doGet</code>.
+ *
+ * 
+ * @author	Lawrie Griffiths
+ *
+ */
+  public class HttpServletResponse {
+  private OutputStream os;
+
+  /**
+   * Creates the Servlet Response object    
+   */
   public HttpServletResponse(OutputStream os) {
     this.os = os;
   }
 
+  /**
+   * Returns an OutputStream for writing the response    
+   * @return  the OutputStream
+   */
   public OutputStream getOutputStream() {
     return os;
   }
 
-  private static final char [] resp = {'H','T','T','P','/','1','.','0',' ','2','0','0','\r','\n',
-                  'C','o','n','t','e','n','t','-','T','y','p','e',':',' '};
-  private static final char [] crlf2 = {'\r','\n','\r','\n'};
+  private static String resp = "HTTP/1.0 200\r\nContent-Type: ";
+  private static String crlf2 = "\r\n\r\n";
 
-  private void send(char [] ca) throws IOException {
+  private void send(String s) throws IOException {
+    char [] ca = s.toCharArray();
     for(int i=0;i<ca.length;i++) os.write((byte) ca[i]); 
   }
 
-  public void setContentType(char [] type) throws IOException {
+  /**
+   * Set the Content Type.
+   * On lejos you must call this method, and you must
+   * call it before writing anything to the output stream.
+   *    
+   * @param type the Mime type, usually text/html
+   */
+  public void setContentType(String type) throws IOException {
     send(resp);
     send(type);
     send(crlf2);
