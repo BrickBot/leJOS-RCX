6e52a57cd7bdd535a8070632b2332c5c77772f0e
lgriffiths <lgriffiths@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-06-01 15:00:49 +0000

    Added charAt and length and made characters package protected to support StringUtils. Also added documentation of all methods.

--- trunk/lejos/src/java/classes/java/lang/String.java
+++ trunk/lejos/src/java/classes/java/lang/String.java
@@ -8,14 +8,42 @@ public final class String
   // NOTE: The state of this class is mapped to
   // native code (see vmsrc/classes.h).
 
-  private char[] characters;
+  char[] characters; // package protected, so StringUtils can access it
 
+  /**
+   * Create a String from a character array.
+   * @param c the character array
+   * @param off the offset - usually 0
+   * @param len the length of the String - must not be greater than c.length
+   **/
   public String (char[] c, int off, int len)
   {
     characters = new char[len];
     System.arraycopy (c, off, characters, 0, len);
   }
 
+  /**
+   * Return the length of the String in characters
+   * @return the length of the String
+   **/
+  public int length()
+  {
+    return characters.length;
+  }
+
+  /**
+   * Return the character at the given index
+   * @return the characters at the given index
+   **/
+  public char charAt(int index) 
+  {
+    return characters[index];
+  }
+
+  /**
+   * Converts the String into an array of characters
+   * @return the character Array
+   **/
   public char[] toCharArray()
   {
     int len = characters.length;
@@ -24,6 +52,10 @@ public final class String
     return ca;
   }
 
+  /**
+   * Converts an Object to a String
+   * @return the String that represents the object
+   **/
   public static String valueOf (Object aObj)
   {
     return aObj.toString();
@@ -31,12 +63,17 @@ public final class String
 
   /**
    * Returns itself.
+   * @return the String itself
    */
   public String toString()
   {
     return this;
   }
   
+  /**
+   * Compares the String with an Object
+   * @return true if the String is equal to the object, false otherwise
+   **/
   public boolean equals(Object other)
   {
     if (other == null)
