05a962a6bc549a51f95f854daa14fd26a6ad5d59
lgriffiths <lgriffiths@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-07-10 23:34:20 +0000

    Added

---
+++ examples/comms/llc_sensor_reader/ReadSensor.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import josx.rcxcomm.*;
+
+/**
+ * This program uses the josx.rcxcomm package to
+ * communicate over the Lego IR link with a
+ * corresponding program, SensorReader. 
+ * It sends the Sensor ID, and reads back the sensor value 
+ * as a short.
+ * @author Lawrie Griffiths
+ */
+public class ReadSensor {
+
+  public static void main(String[] args) {
+
+    try {
+
+      RCXPort port = new RCXPort();
+
+      InputStream is = port.getInputStream();
+      OutputStream os = port.getOutputStream();
+      DataInputStream dis = new DataInputStream(is);
+      DataOutputStream dos = new DataOutputStream(os);
+
+      System.out.println("Reading Light Sensor");
+      int sendTime = (int)System.currentTimeMillis();
+      for(int i=0;i<20;i++) {
+        dos.writeByte(1);
+        dos.flush();
+
+        int n = dis.readShort();
+
+        System.out.println("Received " + n);
+      }
+      System.out.println("Time = " + ((int)System.currentTimeMillis() -  sendTime));
+    }
+    catch (Exception e) {
+      System.out.println("Exception " + e.getMessage());
+    }
+  }
+}
---
+++ examples/comms/lnpaddr_sensor_reader/LNPAddrReadSensor.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import josx.rcxcomm.*;
+
+/**
+ * This program uses the josx.rcxcomm package to
+ * communicate over the Lego IR link with a
+ * corresponding program, SensorReader. 
+ * It sends the Sensor ID, and reads back the sensor value 
+ * as a short.
+ * @author Lawrie Griffiths
+ */
+public class LNPAddrReadSensor {
+
+  public static void main(String[] args) {
+
+    try {
+
+      RCXLNPAddressingPort port = new RCXLNPAddressingPort((byte) 0x20,(byte) 0x00);
+
+      InputStream is = port.getInputStream();
+      OutputStream os = port.getOutputStream();
+      DataInputStream dis = new DataInputStream(is);
+      DataOutputStream dos = new DataOutputStream(os);
+
+      System.out.println("Reading Light Sensor");
+      int sendTime = (int)System.currentTimeMillis();
+      for(int i=0;i<20;i++) {
+        dos.writeByte(1);
+        dos.flush();
+
+        int n = dis.readShort();
+
+        System.out.println("Received " + n);
+      }
+      System.out.println("Time = " + ((int)System.currentTimeMillis() -  sendTime));
+    }
+    catch (Exception e) {
+      System.out.println("Exception " + e.getMessage());
+    }
+  }
+}
