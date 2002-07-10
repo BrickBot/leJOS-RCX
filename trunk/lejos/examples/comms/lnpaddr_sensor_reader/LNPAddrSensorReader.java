5640383ba3f68bd4366ea4097e54e6a666cf2274
lgriffiths <lgriffiths@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-07-10 23:34:20 +0000

    Added

---
+++ trunk/lejos/examples/comms/lnpaddr_sensor_reader/LNPAddrSensorReader.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import josx.rcxcomm.*;
+import josx.platform.rcx.*;
+
+/**
+  * This is an RCX class that reads a sensor value and returns the value   
+  * to the PC via the IR tower.   
+  * @author Brian Bagnall   
+ */
+public class LNPAddrSensorReader {
+  
+  public static void main(String args[]) {
+    int sensorID, sensorValue;
+    RCXLNPAddressingPort port = null;
+    try {
+      port = new RCXLNPAddressingPort((byte) 0x00, (byte) 0x20);
+      DataOutputStream out = new DataOutputStream(port.getOutputStream());   
+      while (true) {
+        sensorID = port.getInputStream().read();
+        sensorValue = Sensor.readSensorValue(sensorID, 0);
+        LCD.showNumber(sensorValue);
+        out.writeShort(sensorValue);
+        out.flush()
+      }
+    } catch (IOException ioE) {
+      LCD.showNumber(1111);
+    } finally {
+      port.close();
+    }
+  }
+}
+
+
