ff2a1565a49ec47873ae2d6906212d7a96361949
mpandrews <mpandrews@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-08-01 17:30:17 +0000

    Added Activity class to josx.robotics

---
+++ trunk/lejos/src/java/classes/josx/robotics/StopException.java
@@ -0,0 +1,10 @@
+package josx.robotics;
+
+/**
+ * This exception indicates that an activity should stop.
+ * @see Activity
+ * @author Paul Andrews
+ */
+public class StopException extends Exception
+{
+}
