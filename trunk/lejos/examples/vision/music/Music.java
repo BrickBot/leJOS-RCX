bf473fab91460a1257c2581ecd0afbbd006a17e8
lgriffiths <lgriffiths@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-08-18 21:54:51 +0000

    Added

---
+++ trunk/lejos/examples/vision/music/Music.java
@@ -0,0 +1,31 @@
+import josx.vision.*;
+
+public class Music implements MotionListener  {
+  long lastPlay = 0;
+  static String dir;
+    
+  public static void main(String [] args) {
+    dir = args[0];
+    (new Music()).run();
+  }
+
+  private void run() {
+    Vision.setImageSize(320, 240);
+    for(int i=0;i<16;i++) {
+      Vision.addRectRegion(i+1, i * 20, 0, 20, 160);
+      Vision.addMotionListener(i+1, this);
+    }
+    Vision.addRectRegion(17,0,160,320,80);
+    Vision.addMotionListener(17,this);
+    Vision.startViewer("Music");
+  }
+
+  public void motionDetected(int region) {
+    if ((System.currentTimeMillis() - lastPlay) > 1000) {
+      lastPlay = System.currentTimeMillis();
+      if (region == 17) Vision.playSound("../../Effects/Cymbal1.wav");
+      else Vision.playSound("../../" + dir + "/E" + (region < 10 ? "0" + region : "" + region) + ".wav");
+    }   
+  }
+}
+
