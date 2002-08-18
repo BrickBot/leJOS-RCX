5a66792c4abf0bf944fd6d4e759f3bd1768acf54
lgriffiths <lgriffiths@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-08-18 13:05:17 +0000

    Added

---
+++ trunk/lejos/examples/vision/motion/Motion.java
@@ -0,0 +1,26 @@
+import josx.vision.*;
+
+public class Motion implements MotionListener {
+  private static int image = 1;
+  long lastPlay = 0;
+
+  public static void main(String [] args) {
+    (new Motion()).run();
+  }
+
+  private void run() {
+    Vision.setImageSize(320, 240);
+    Vision.addRectRegion(1, 0, 0, 320, 240);
+    Vision.addMotionListener(1, this);
+    Vision.startViewer("Test Vision");
+  }
+
+  public void motionDetected(int region) {
+    if ((System.currentTimeMillis() - lastPlay) > 1000) {
+      lastPlay = System.currentTimeMillis();
+      Vision.snapshot("Intruder" + image++ + ".jpg");
+      Vision.playSound("Effects/Alarm.wav");
+    }   
+  }
+}
+
