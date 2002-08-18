d852fe1f07e89698e47d04eee0585d5c58252324
lgriffiths <lgriffiths@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-08-18 22:37:03 +0000

    Added

---
+++ trunk/lejos/examples/vision/alarm/Alarm.java
@@ -0,0 +1,38 @@
+import josx.vision.*;
+
+public class Alarm implements MotionListener, ColorListener {
+  long lastPlay = 0;
+  private final int blue = 0x20a0c0;
+
+  public static void main(String [] args) {
+    (new Alarm()).run();
+  }
+
+  private void run() {
+    Vision.setImageSize(320, 240);
+    Vision.addRectRegion(1, 30, 100, 50, 100);
+    Vision.addMotionListener(1, this);
+    Vision.addRectRegion(2, 130, 100, 50, 100);
+    Vision.addMotionListener(2, this);
+    Vision.addRectRegion(3, 230, 100, 50, 100);
+    Vision.addColorListener(3, this, blue);
+    Vision.captureColor(true);
+    Vision.startViewer("Alarm");
+  }
+
+  public void motionDetected(int region) {
+    if ((System.currentTimeMillis() - lastPlay) > 1000) {
+      lastPlay = System.currentTimeMillis();
+      Vision.playSound("../../Effects/Alarm.wav");
+    }   
+  }
+  public void colorDetected(int region, int color) {
+    if ((System.currentTimeMillis() - lastPlay) > 1000) {
+      lastPlay = System.currentTimeMillis();
+      Vision.playSound("../../Effects/Cavalry.wav");
+      Vision.stopViewer();
+      System.exit(0);
+    }   
+  }
+}
+
