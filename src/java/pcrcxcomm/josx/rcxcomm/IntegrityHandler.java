3fe8fbcb01b3423f32cade4749c29f07fbed4229
lgriffiths <lgriffiths@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-09-20 22:18:25 +0000

    Initial version

---
+++ src/java/pcrcxcomm/josx/rcxcomm/IntegrityHandler.java
@@ -0,0 +1,15 @@
+package josx.rcxcomm;
+
+/**
+ * An interface for processing incoming broadcasts of LNP integrity packets.
+ **/
+public interface IntegrityHandler {
+  /**
+   * Process the broadcast integrity packet.
+   * @param packet the broadcast packet
+   * @param len the length of the packet
+   **/
+  void processIntegrityPacket (byte [] packet, int len);
+}
+ 
+
