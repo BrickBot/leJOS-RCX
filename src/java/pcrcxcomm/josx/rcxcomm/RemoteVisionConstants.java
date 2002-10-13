ef090cbd9c9d4c4cef8d9215a88ed5f456325e0b
lgriffiths <lgriffiths@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-10-13 22:00:58 +0000

    Initial version of RCX remote control

---
+++ src/java/pcrcxcomm/josx/rcxcomm/RemoteVisionConstants.java
@@ -0,0 +1,28 @@
+package josx.rcxcomm;
+
+public interface RemoteVisionConstants {
+
+  static final byte METHOD_JOSX_VISION_RCX_FORWARD_V = 100;
+  static final byte METHOD_JOSX_VISION_RCX_BACKWARD_V = 101;
+  static final byte METHOD_JOSX_VISION_RCX_STOP = 102;
+  static final byte METHOD_JOSX_VISION_RCX_SPIN_LEFT_V = 103;
+  static final byte METHOD_JOSX_VISION_RCX_SPIN_RIGHT_V = 104;
+  static final byte METHOD_JOSX_VISION_RCX_FORWARD_I = 105;
+  static final byte METHOD_JOSX_VISION_RCX_BACKWARD_I = 106;
+  static final byte METHOD_JOSX_VISION_RCX_SPIN_LEFT_I = 107;
+  static final byte METHOD_JOSX_VISION_RCX_SPIN_RIGHT_I = 108;
+  static final byte METHOD_JOSX_VISION_RCX_TURN_LEFT_V = 109;
+  static final byte METHOD_JOSX_VISION_RCX_TURN_LEFT_I = 110;
+  static final byte METHOD_JOSX_VISION_RCX_TURN_RIGHT_V = 111;
+  static final byte METHOD_JOSX_VISION_RCX_TURN_RIGHT_I = 112;
+  static final byte METHOD_JOSX_VISION_RCX_SCAN_LEFT_V = 113;
+  static final byte METHOD_JOSX_VISION_RCX_SCAN_LEFT_I = 114;
+  static final byte METHOD_JOSX_VISION_RCX_SCAN_RIGHT_V = 115;
+  static final byte METHOD_JOSX_VISION_RCX_SCAN_RIGHT_I = 116;
+  static final byte METHOD_JOSX_VISION_RCX_SET_POWER = 117;
+  static final byte METHOD_JOSX_VISION_RCX_CONTROL_MOTORS = 118;
+  static final byte METHOD_JOSX_VISION_RCX_PLAY_TONE = 119;
+  static final byte METHOD_JOSX_VISION_RCX_TILT_UP_I = 120;
+  static final byte METHOD_JOSX_VISION_RCX_TILT_DOWN_I = 121;
+}
+
