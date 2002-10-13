d9562e20dc663b8950de861817943fccb5fef097
lgriffiths <lgriffiths@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-10-13 22:02:46 +0000

    Initial version of RCX remote method execution

---
+++ trunk/lejos/src/java/pcrcxcomm/josx/rcxcomm/RCXRemote.java
@@ -0,0 +1,41 @@
+package josx.rcxcomm;
+
+import josx.rcxcomm.*;
+import java.io.*;
+
+/**
+ * Utility class to support remote execution.
+ * Creates an RCXPort for communication.
+ * public data stream variables in anfd out are used 
+ * to send add receive data. 
+ **/
+public class RCXRemote {
+  private static RCXPort port;
+  public static DataInputStream in;
+  public static DataOutputStream out;
+
+  static {
+    try {
+      System.out.println("Starting remoting");
+      start();
+    } catch (IOException e) {
+      System.out.println("Remoting failed");
+    }
+    System.out.println("Remoting started");
+  }
+
+  public static void start() throws IOException {
+    port = new RCXPort();
+    in = new DataInputStream(port.getInputStream());
+    out = new DataOutputStream(port.getOutputStream());
+  }
+
+  public static void stop() {
+    port.close();
+  }
+
+  public static void error() {
+    System.out.println("Error in remote execution");
+  }
+}
+
