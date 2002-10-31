cc7bb9109c1f9df5ae4995cc03d42dd8483a7df3
lgriffiths <lgriffiths@4432cd44-f382-4be9-9dff-e810c426dc2b>
2002-10-31 22:17:18 +0000

    Better option processing

--- trunk/lejos/src/java/tools/js/tools/Lejosdl.java
+++ trunk/lejos/src/java/tools/js/tools/Lejosdl.java
@@ -11,38 +11,46 @@ public class Lejosdl {
   private static final int MAGIC = 0xCAF6;
 
   public static void main(String [] args) throws IOException {
-    String fileName = args[0];
+    String fileName = "";
     FileInputStream fis = null;
     long pLength = 0;
     byte [] buffer;
     boolean usage = false;
     String tty = "";
+    int i;
  
-   // Process args
+    // Process args
 
-    for(int i=0; i < args.length;i++) {
+   for(i=0; i < args.length && args[i].substring(0,1).equals("-");i++) {
       if (args[i].equals("--tty") && i < args.length - 1) {
         tty = args[++i];
+        System.out.println("Setting tty = " + tty);
+      } else if(args[i].length() > 6 && args[i].substring(0,6).equals("--tty=")) {
+        tty = args[i].substring(6);
         System.out.println("Setting tty = " + tty);
       } else if (args[i].equals("--help") || args[i].equals("-h")) {
         usage = true;
       } else if (args[i].equals("--debug")) {
-        System.out.println("For debug output set RCXCOMM_DEBUG=Y");
+        System.err.println("For debug output set RCXCOMM_DEBUG=Y");
+        System.exit(1);
+      } else {
+        System.err.println("Unrecognized option " + args[i]);
         System.exit(1);
       }
     }
 
+    if (i <args.length) fileName = args[i];
+    else usage = true;
+
     if (usage) {
-      System.out.println("usage: firmdl [options]");
-      System.out.println("--tty=<tty>   assume tower connected to <tty>");
-      System.out.println("--tty=usb     assume tower connected to usb");
-      System.out.println("-h, --help    display this message and exit");
-      System.out.println("For debug output set RCXCOMM_DEBUG=Y");
+      System.err.println("usage: lejosdl [options] filename");
+      System.err.println("--tty=<tty>   assume tower connected to <tty>");
+      System.err.println("--tty=usb     assume tower connected to usb");
+      System.err.println("-h, --help    display this message and exit");
+      System.err.println("For debug output set RCXCOMM_DEBUG=Y");
       System.exit(1);
     }
    
-    Download.open("");
-
     // Open the file
 
     try {
@@ -50,21 +58,23 @@ public class Lejosdl {
       pLength = f.length();
       fis = new FileInputStream(f);
     } catch (FileNotFoundException fe) {
-      System.out.println("File " + fileName + " not found");
+      System.err.println("File " + fileName + " not found");
       System.exit(1);
     }
 
     if (pLength > 0xFFFF) {
-      System.out.println("Huge file: " + pLength + " bytes");
+      System.err.println("Huge file: " + pLength + " bytes");
       System.exit (1);
     }
+
+    Download.open(tty);
 
     buffer = new byte[(int) pLength];
     fis.read(buffer);
 
     if (buffer[0] != (byte) ((MAGIC >> 8) & 0xFF) ||
       buffer[1] != (byte) ((MAGIC >> 0) & 0xFF)) {
-      System.out.println("Magic number is not right. Linker used was for emulation only?");
+      System.err.println("Magic number is not right. Linker used was for emulation only?");
       System.exit (1);
     }
 
