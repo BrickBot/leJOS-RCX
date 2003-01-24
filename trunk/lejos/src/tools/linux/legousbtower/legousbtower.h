9a2e9e4098ef3b4ac2a88b40bc2bbbbf16527039
davidgsf <davidgsf@4432cd44-f382-4be9-9dff-e810c426dc2b>
2003-01-24 09:27:40 +0000

    Added timeout defines for reads and write in driver

---
+++ trunk/lejos/src/tools/linux/legousbtower/legousbtower.h
@@ -0,0 +1,9 @@
+#ifndef __LEGOUSBTOWER_H__
+#define __LEGOUSBTOWER_H__
+
+
+#define LEGO_TOWER_SET_READ_TIMEOUT _IOW('u', 0xc8, int)
+#define LEGO_TOWER_SET_WRITE_TIMEOUT _IOW('u', 0xc9, int)
+
+
+#endif
