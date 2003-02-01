c61573dcb5266702cd425fdb80f1adb3511b33c9
lgriffiths <lgriffiths@4432cd44-f382-4be9-9dff-e810c426dc2b>
2003-02-01 11:22:00 +0000

    Make download tool optional

--- bin/lejoslink.bat
+++ bin/lejoslink.bat
@@ -3,7 +3,7 @@ set CLASSPATH_SAVE=%CLASSPATH%
 if "%LEJOS_HOME%" == ""  goto homeless
 
 set CLASSPATH=%LEJOS_HOME%\lib\jtools.jar;%CLASSPATH%
-java -Dtinyvm.linker=lejoslink -Dtinyvm.home=%LEJOS_HOME% -Dtinyvm.write.order=BE -Dtinyvm.loader=lejosrun js.tinyvm.TinyVM -classpath %LEJOS_HOME%\lib\classes.jar;%LEJOS_HOME%\lib\rcxrcxcomm.jar;%CLASSPATH% %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Dtinyvm.linker=lejoslink -Dtinyvm.home=%LEJOS_HOME% -Dtinyvm.write.order=BE js.tinyvm.TinyVM -classpath %LEJOS_HOME%\lib\classes.jar;%LEJOS_HOME%\lib\rcxrcxcomm.jar;%CLASSPATH% %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto end
 
 :homeless
