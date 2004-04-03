0c6a57fdf0e861a9f95755ca91c88a13b6494de8
starblue <starblue@4432cd44-f382-4be9-9dff-e810c426dc2b>
2004-04-03 11:10:19 +0000

    added message examples

---
+++ trunk/lejos/examples/comms/message/sender/Sender.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import josx.platform.rcx.*;
+
+/**
+  * This is an RCX class that sends messages to other RCXs.
+  * @author Jürgen Stuber
+  */
+public class Sender {
+  
+    public static void main(String args[])
+    {
+	byte message = 1;
+
+	while (Button.RUN.isPressed()) {
+	}
+
+	while (!Button.RUN.isPressed()) {
+	    LCD.showNumber(message & 255);
+	    Message.send (message);
+	    if (message < 255) {
+		message++;
+	    } else {
+		message = 1;
+	    }
+	    try {
+		Thread.sleep(200);
+	    } catch (InterruptedException e) {
+	    }
+	}
+
+	while (Button.RUN.isPressed()) {
+	}
+    }
+}
+
+
