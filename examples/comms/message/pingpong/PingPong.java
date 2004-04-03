412f403258087849860787bc76f03094d7e628cf
starblue <starblue@4432cd44-f382-4be9-9dff-e810c426dc2b>
2004-04-03 11:10:19 +0000

    added message examples

---
+++ examples/comms/message/pingpong/PingPong.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+import josx.platform.rcx.*;
+
+/**
+  * This is an RCX class that sends and receives messages to other RCXs
+  * using the SetMessage opcode.  Load it on any number of RCXs and
+  * start them, they will collaboratively cout upwards.
+  * @author Jürgen Stuber
+  */
+public class PingPong
+{
+    private static Random r = new Random();
+
+    public static void main(String args[])
+    {
+	byte count = 1;
+
+	while (Button.RUN.isPressed()) {
+	}
+
+	while (!Button.RUN.isPressed()) {
+	    int timeout = (int)System.currentTimeMillis() + delay();
+	    while (!Message.isAvailable()
+		   && (int)System.currentTimeMillis() < timeout) {
+		Thread.yield();
+	    }
+	    if (Message.isAvailable()) {
+		byte message = Message.get();
+		Message.clear();
+		if ((byte)(message - count) >= 0) {
+		    count = (byte)(message + 1);
+		    if (count == 0) {
+			    count++;
+		    }
+		}
+	    }
+	    try {
+		Thread.sleep(delay());
+	    } catch (InterruptedException e) {
+	    }
+	    LCD.showNumber(count & 255);
+	    Message.send (count);
+	}
+
+	while (Button.RUN.isPressed()) {
+	}
+    }
+
+    private static int delay() {
+	int delay = 10;
+	while (r.nextInt(3) != 0) {
+	    delay += 40;
+	}
+	return delay;
+    }
+}
+
+
---
+++ examples/comms/message/receiver/Receiver.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import josx.platform.rcx.*;
+
+/**
+  * This is an RCX class that receives messages from other RCXs.
+  * @author Jürgen Stuber
+  */
+public class Receiver {
+  
+    public static void main(String args[])
+    {
+	while (Button.RUN.isPressed()) {
+	}
+
+	while (!Button.RUN.isPressed()) {
+	    while (!Message.isAvailable() && !Button.RUN.isPressed()) {
+		Thread.yield();
+	    }
+	    LCD.showNumber(Message.get() & 255);
+	}
+
+	while (Button.RUN.isPressed()) {
+	}
+    }
+}
+
+
