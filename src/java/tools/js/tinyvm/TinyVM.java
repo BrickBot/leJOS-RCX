2bbf671d57bf75c1548166465ce9ac930888f82c
markus_heiden <markus_heiden@4432cd44-f382-4be9-9dff-e810c426dc2b>
2004-06-18 22:06:39 +0000

    extracted gameboy code from tiny vm

--- src/java/tools/js/tinyvm/TinyVM.java
+++ src/java/tools/js/tinyvm/TinyVM.java
@@ -1,12 +1,8 @@
 package js.tinyvm;
 
 import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -25,20 +21,10 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 /**
- * Tiny VM
- * 
- * TODO use _progress instead of Assertion TODO refactor commandline parsing
- * TODO use no system properties, use parameters instead
+ * Tiny VM.
  */
 public class TinyVM implements Constants
 {
-  static final String CP_PROPERTY = "tinyvm.class.path";
-  static final String TINYVM_HOME = System.getProperty("tinyvm.home");
-  static final String TINYVM_LOADER = System.getProperty("tinyvm.loader");
-  static final String TEMP_DIR = System.getProperty("temp.dir");
-  static final String TEMP_FILE = "__tinyvm__temp.tvm__";
-  static final String TOOL_NAME = System.getProperty("tinyvm.linker");
-
   private ToolProgressListener _progress = null;
 
   private boolean _verbose = false;
@@ -57,7 +43,6 @@ public class TinyVM implements Constants
     }
     catch (TinyVMException e)
     {
-      e.printStackTrace();
       System.err.println(e.getMessage());
       System.exit(1);
     }
@@ -85,11 +70,9 @@ public class TinyVM implements Constants
     options.addOption("v", "verbose", false,
         "print class and signature information");
     options.addOption("cp", "classpath", true, "classpath");
-    options.addOption("d", "download", false, "download binary");
     options.addOption("o", "output", true, "dump binary into path");
-    options.addOption("a", "all", false, "???");
+    options.addOption("a", "all", false, "do not filter classes");
     options.addOption("wo", "writeorder", true, "write order (BE or LE)");
-    options.addOption("gb", "gameboy", false, "dump gameboy rom");
 
     CommandLine commandLine;
     try
@@ -104,38 +87,20 @@ public class TinyVM implements Constants
 
     // options
     _verbose = commandLine.hasOption("v");
-    String systemClasspath = System.getProperty(CP_PROPERTY);
-    String classpath = commandLine.getOptionValue("cp", systemClasspath);
-    boolean download = commandLine.hasOption("d");
+    String classpath = commandLine.getOptionValue("cp");
     String output = commandLine.getOptionValue("o");
     boolean all = commandLine.hasOption("a");
     boolean bigEndian = "be".equalsIgnoreCase(commandLine.getOptionValue("wo"));
-    boolean dumpGameboy = commandLine.hasOption("gb");
 
     // files
     String[] classes = commandLine.getArgs();
 
     try
     {
-      if (dumpGameboy)
-      {
-        ByteArrayOutputStream stream = new ByteArrayOutputStream(1024);
-        link(classpath, classes, all, stream, bigEndian);
-        downloadGameboyRom(stream, classes[0]);
-      }
-      else if (download)
-      {
-        File file = new File(new File(TEMP_DIR), TEMP_FILE);
-        OutputStream stream = new FileOutputStream(file);
-        link(classpath, classes, all, stream, bigEndian);
-        invokeTvm(output);
-        file.delete();
-      }
-      else if (output != null)
-      {
-        OutputStream stream = new FileOutputStream(output);
-        link(classpath, classes, all, stream, bigEndian);
-      }
+      OutputStream stream = output == null
+          ? (OutputStream) System.out
+          : (OutputStream) new FileOutputStream(output);
+      link(classpath, classes, all, stream, bigEndian);
     }
     catch (FileNotFoundException e)
     {
@@ -157,24 +122,15 @@ public class TinyVM implements Constants
   {
     try
     {
-      if (!commandLine.hasOption("cp")
-          && System.getProperty(CP_PROPERTY) == null)
+      if (!commandLine.hasOption("cp"))
       {
         throw new TinyVMException("No classpath defined");
       }
 
-      int dumpGameboyRom = commandLine.hasOption("gb") ? 1 : 0;
-      int download = commandLine.hasOption("d") ? 1 : 0;
-      int output = commandLine.hasOption("o") ? 1 : 0;
-      if (dumpGameboyRom + download + output > 1)
+      if (!commandLine.hasOption("wo"))
       {
-        throw new TinyVMException("-gb -d and -o are exclusive");
+        throw new TinyVMException("No write order specified");
       }
-      else if (dumpGameboyRom + download + output == 0)
-      {
-        throw new TinyVMException("No output location specified");
-      }
-
       String writeOrder = commandLine.getOptionValue("wo").toLowerCase();
       if (!"be".equals(writeOrder) && !"le".equals(writeOrder))
       {
@@ -193,6 +149,7 @@ public class TinyVM implements Constants
       printWriter.println(e.getMessage());
 
       String usage = getClass().getName() + " [options] class1[,class2,...]\n";
+      // TODO check format parameters
       new HelpFormatter().printHelp(printWriter, 80, usage.toString(), null,
           options, 0, 2, null);
 
@@ -283,110 +240,5 @@ public class TinyVM implements Constants
       // TODO make other classes throw TinyVMExceptions too
       throw new TinyVMException(e);
     }
-  }
-
-  /**
-   * Execute tiny vm.
-   * 
-   * @param aEntryClasses
-   * @throws Exception
-   */
-  public void downloadGameboyRom (ByteArrayOutputStream stream, String mainClass)
-      throws TinyVMException
-  {
-    try
-    {
-      GbMerger merger = new GbMerger(TEMP_DIR);
-      merger.dumpRom(stream.toByteArray(), mainClass);
-    }
-    catch (Exception e)
-    {
-      // TODO make other classes throw TinyVMExceptions too
-      throw new TinyVMException(e);
-    }
-  }
-
-  /**
-   * Execute tiny vm.
-   * 
-   * @param aFileName ???
-   * @throws TinyVMException
-   * @throws TinyVMException
-   */
-  public void invokeTvm (String aFileName) throws TinyVMException
-  {
-    if (TINYVM_HOME == null)
-    {
-      throw new TinyVMException("tinyvm.home not set");
-    }
-
-    // Allow download tool to be null
-
-    if (TINYVM_LOADER == null)
-      return;
-
-    String pTvmExec = TINYVM_HOME + File.separator + "bin" + File.separator
-        + TINYVM_LOADER;
-    String[] pParams = new String[]
-    {pTvmExec, aFileName};
-    try
-    {
-      if (_verbose)
-      {
-        _progress.log("Executing " + pTvmExec + " (downloading) ...");
-      }
-      Process p = Runtime.getRuntime().exec(pParams);
-      pipeStream(p.getInputStream(), System.out);
-      pipeStream(p.getErrorStream(), System.err);
-      int pStatus;
-      if ((pStatus = p.waitFor()) != 0)
-      {
-        System.err
-            .println(TINYVM_LOADER + ": returned status " + pStatus + ".");
-      }
-      // Hack: Small wait to get all the output flushed.
-      Thread.sleep(100);
-      System.out.flush();
-      System.err.flush();
-    }
-    catch (InterruptedException e)
-    {
-      throw new TinyVMException("Execution of " + pTvmExec
-          + " was interrupted.");
-    }
-    catch (IOException e)
-    {
-      throw new TinyVMException("Problem executing " + pTvmExec + ". "
-          + "Apparently, the program was not found. ");
-    }
-  }
-
-  static void pipeStream (final InputStream aIn, final OutputStream aOut)
-  {
-    Thread pThread = new Thread("output-pipe")
-    {
-      public void run ()
-      {
-        try
-        {
-          int c;
-          for (;;)
-          {
-            c = aIn.read();
-            if (c == -1)
-              Thread.sleep(1);
-            else
-              aOut.write(c);
-          }
-        }
-        catch (Exception e)
-        {
-          e.printStackTrace();
-        }
-      }
-    };
-
-    pThread.setDaemon(true);
-    pThread.start();
   }
 }
---
+++ src/java/tools/js/tinyvm/gameboy/GameboyMerger.java
@@ -0,0 +1,71 @@
+package js.tinyvm.gameboy;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+public class GameboyMerger
+{
+  String tempDir;
+  String compiler;
+
+  public GameboyMerger(String tmpDir)
+  {
+    tempDir = tmpDir == null ? "." : tmpDir;
+    compiler = "lcc";
+  }
+
+  String getByteCodeFile (String className)
+  {
+    File dirFile = new File(tempDir);
+    return new File(dirFile, "gb$" + className + ".c").getPath();
+  }
+
+  String getRomFile (String className)
+  {
+    return className + ".gb";
+  }
+
+  public void dumpRom (InputStream stream, String className) throws IOException
+  {
+    String cfile = getByteCodeFile(className);
+    dumpByteCodeFile(stream, cfile);
+    String outfile = getRomFile(className);
+    //invokeLinker (cfile, outfile);
+  }
+
+  void dumpByteCodeFile (InputStream stream, String outputFile)
+      throws IOException
+  {
+    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
+        outputFile), 4096));
+    out.println("/* Machine generated temp file */");
+    out.println();
+    out.println("#include <asm/types.h>");
+    out.println();
+    // The name "lejos_code" also appears in main.c
+    out.println("UINT8 lejos_code[] = {");
+    dumpByteCodeList(stream, out);
+    out.println("};");
+    out.close();
+  }
+
+  void dumpByteCodeList (InputStream stream, PrintWriter out)
+      throws IOException
+  {
+    int b;
+    for (int i = 0; (b = stream.read()) != -1; i++)
+    {
+      out.print("0x" + Integer.toHexString(((byte) b) & 0xFF));
+      out.print(", ");
+      if (i % 10 == 0)
+      {
+        out.println();
+      }
+    }
+  }
+}
+
