818f1d50b1a270464b4d7b9841c4f0d601a6590e
markus_heiden <markus_heiden@4432cd44-f382-4be9-9dff-e810c426dc2b>
2004-06-23 21:44:33 +0000

    added first try of bcel base tiny vm

---
+++ trunk/lejos/src/java/tools/js/tinyvm2/StaticFieldRecord.java
@@ -0,0 +1,60 @@
+package js.tinyvm2;
+
+import java.io.IOException;
+
+import js.tinyvm.io.ByteWriter;
+
+import org.apache.bcel.classfile.Field;
+
+public class StaticFieldRecord implements WritableData
+{
+   ClassRecord iClassRecord;
+   Field iField;
+
+   public StaticFieldRecord (Field aEntry, ClassRecord aRec)
+   {
+      iField = aEntry;
+      iClassRecord = aRec;
+   }
+
+   public String getName ()
+   {
+      return iField.getName();
+   }
+
+   public int getLength ()
+   {
+      return 2;
+   }
+
+   public void dump (ByteWriter aOut) throws TinyVMException
+   {
+      int pType = iField.getType().getType();
+      assert pType >= 0 && pType <= 0xF: "Check: valid type";
+      int pOffset = iClassRecord.getStaticFieldOffset(iField.getName());
+      assert pOffset >= 0 && pOffset <= 0x0FFF: "Check offset in range";
+
+      try
+      {
+         aOut.writeU2((pType << 12) | pOffset);
+      }
+      catch (IOException e)
+      {
+         throw new TinyVMException(e.getMessage(), e);
+      }
+   }
+
+   public boolean equals (Object aOther)
+   {
+      if (!(aOther instanceof StaticFieldRecord))
+         return false;
+      return ((StaticFieldRecord) aOther).iField.equals(iField)
+         && ((StaticFieldRecord) aOther).iClassRecord.equals(iClassRecord);
+   }
+
+   public int hashCode ()
+   {
+      return iField.hashCode();
+   }
+}
+
---
+++ trunk/lejos/src/java/tools/js/tinyvm2/WritableData.java
@@ -0,0 +1,13 @@
+package js.tinyvm2;
+
+import js.tinyvm.io.ByteWriter;
+
+public interface WritableData
+{
+   public void dump (ByteWriter aOut) throws TinyVMException;
+
+   /**
+    * Returns the length of the record, in bytes.
+    */
+   public int getLength () throws TinyVMException;
+}
