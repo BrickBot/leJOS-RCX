03ee800f343460ca27dda0df3bc5809c7b78d379
markus_heiden <markus_heiden@4432cd44-f382-4be9-9dff-e810c426dc2b>
2004-07-18 12:16:21 +0000

    mh: rework

--- trunk/lejos/src/java/tools/js/tinyvm/io/IOUtilities.java
+++ trunk/lejos/src/java/tools/js/tinyvm/io/IOUtilities.java
@@ -2,16 +2,32 @@ package js.tinyvm.io;
 
 import java.io.IOException;
 
+/**
+ * Helper class for padding.
+ */
 public class IOUtilities
 {
-   public static void writePadding (IByteWriter aOut, int aMinRecSize)
+   /**
+    * Write padding bytes.
+    * 
+    * @param writer writer to write to
+    * @param aMinRecSize size of one record
+    * @throws IOException
+    */
+   public static void writePadding (IByteWriter writer, int aMinRecSize)
       throws IOException
    {
-      int pRegSize = aOut.offset();
+      int pRegSize = writer.offset();
       int pPad = adjustedSize(pRegSize, aMinRecSize) - pRegSize;
-      aOut.write(new byte[pPad]);
+      writer.write(new byte[pPad]);
    }
 
+   /**
+    * Size in bytes corrected to record boundaries.
+    * 
+    * @param aSize current size
+    * @param aMinRecSize size after padding
+    */
    public static int adjustedSize (int aSize, int aMinRecSize)
    {
       int pMod = aSize % aMinRecSize;
