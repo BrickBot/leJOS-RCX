86136f2676344459922baadd8bdbbe3c7543a962
markus_heiden <markus_heiden@4432cd44-f382-4be9-9dff-e810c426dc2b>
2004-07-21 22:12:26 +0000

    mh: added gcc source and a script for generating a h8300 cross compiler

---
+++ trunk/lejos/src/h8300-compiler/install.sh
@@ -0,0 +1,49 @@
+@echo ""
+@echo "====> extract binutils"
+@echo ""
+tar xfz binutils-2.13.2.tar.gz
+@echo ""
+@echo "====> build binutils"
+@echo ""
+cd binutils-2.13.2
+mkdir build_binutils
+cd build_binutils
+../configure --prefix=/usr/local --target=h8300-hms
+make CFLAGS="-O2 -fomit-frame-pointer" all
+make install
+cd ../..
+
+@echo ""
+@echo "====> extract gcc"
+@echo ""
+export PATH=$PATH:/usr/local/bin
+tar xfz gcc-core-3.2.1.tar.gz
+tar xfz newlib-1.11.0.tar.gz
+@echo ""
+@echo "====> build gcc"
+@echo ""
+cd gcc-3.2.1
+ln -s ../newlib-1.11.0/newlib .
+patch -p1 < ../h8300-hms-gcc-3.1-1.patch
+mkdir build_gcc
+cd build_gcc
+../configure --prefix=/usr/local --target=h8300-hms --enable-languages=c --with-newlib
+make CFLAGS="-O2 -fomit-frame-pointer" all
+make install
+cd ../..
+
+@echo ""
+@echo "====> extract gdb"
+@echo ""
+tar xfz gdb-5.2.1.tar.gz
+@echo ""
+@echo "====> build gdb"
+@echo ""
+cd gdb-5.2.1
+mkdir build_gdb
+cd build_gdb
+../configure --prefix=/usr/local --target=h8300-hms
+make CFLAGS="-O2 -fomit-frame-pointer" all
+make install
+cd ../..
+
