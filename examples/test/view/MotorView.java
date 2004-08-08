bbeaaba415e0ce4a88cb0e6e72254754021af547
starblue <starblue@4432cd44-f382-4be9-9dff-e810c426dc2b>
2004-08-08 09:27:49 +0000

    removed unmaintained build.sh, use make or ant instead

--- examples/test/view/MotorView.java
+++ examples/test/view/MotorView.java
@@ -1,85 +1,91 @@
 import josx.platform.rcx.*;
 
-public class MotorView extends PortView
+public class MotorView implements PortView, LCDConstants, Segment
 {
-  static final int MAXSTATE = 3;
-  static final int MAXPOWER = 7;
+    static final int MAXSTATE = 3;
+    static final int MAXPOWER = 7;
 
-  public static final MotorView A = new MotorView( Motor.A, 0);
-  public static final MotorView B = new MotorView( Motor.B, 1);
-  public static final MotorView C = new MotorView( Motor.C, 2);
+    public static final MotorView A = new MotorView( Motor.A, 0);
+    public static final MotorView B = new MotorView( Motor.B, 1);
+    public static final MotorView C = new MotorView( Motor.C, 2);
 
-  final Motor motor;
+    private static final int[] MOTOR_VIEW = 
+	new int[] { MOTOR_A_VIEW, MOTOR_B_VIEW, MOTOR_C_VIEW};
+    private static final int[] MOTOR_FORWARD = 
+	new int[] { MOTOR_A_FWD, MOTOR_B_FWD, MOTOR_C_FWD};
+    private static final int[] MOTOR_BACKWARD = 
+	new int[] { MOTOR_A_REV, MOTOR_B_REV, MOTOR_C_REV};
 
-  int iState;
-  int iPower;
+    final Motor motor;
 
-  MotorView( Motor m, int n )
-  {
-    super( true, n);
-    motor = m;
+    int number;
+    int state;
+    int power;
 
-    iState = 0;
-    iPower = 7;
-    setState();
-    setPower();
-  }
+    MotorView( Motor m, int n )
+    {
+	motor = m;
+	number = n;
 
-  void setPower()
-  {
-    motor.setPower(iPower);
-  }
-
-  void setState()
-  {
-    if (iState == 0)
-      motor.flt();
-    else if (iState == 1)
-      motor.forward();
-    else if (iState == 2)
-      motor.backward();
-    else if (iState == 3)
-      motor.stop();
-  }
-
-  public void showPort()
-  {
-    if (iState == 0)
-      {}
-    else if (iState == 1)
-      LCD.setSegment( LCDSegment.motorForward( number));
-    else if (iState == 2)
-      LCD.setSegment( LCDSegment.motorBackward( number));
-    else if (iState == 3){
-      LCD.setSegment( LCDSegment.motorForward( number));
-      LCD.setSegment( LCDSegment.motorBackward( number));
+	state = 0;
+	power = 7;
+	setState();
+	setPower();
     }
-  }
 
-  public void showValues()
-  {
-    LCDProgramNumber.set (iState);
-    LCDNumber.set (iPower, 0, true);
-  }
+    void setPower()
+    {
+	motor.setPower(power);
+    }
 
-  public void runPressed()
-  {
-    iState++;
-    if (iState > MAXSTATE)
-      iState = 0;
-    setState();
-  }
+    void setState()
+    {
+	if (state == 0)
+	    motor.flt();
+	else if (state == 1)
+	    motor.forward();
+	else if (state == 2)
+	    motor.backward();
+	else if (state == 3)
+	    motor.stop();
+    }
 
-  public void prgmPressed()
-  {
-    iPower++;
-    if (iPower > MAXPOWER)
-      iPower = 0;
-    setPower();
-  }
+    public void showCursor()
+    {
+	LCD.setSegment (MOTOR_VIEW[number]);
+    }
 
-  public void shutdown()
-  {
-    motor.stop();
-  }
+    public void showPort()
+    {
+	if (state == 1 || state == 3) {
+	    LCD.setSegment (MOTOR_FORWARD[number]);
+	}
+	if (state == 2 || state == 3) {
+	    LCD.setSegment (MOTOR_BACKWARD[number]);
+	}
+    }
+
+    public void showValues()
+    {
+	LCD.setNumber ( LCD_PROGRAM, power, 0);
+    }
+
+    public void runPressed()
+    {
+	state++;
+	if (state > MAXSTATE)
+	    state = 0;
+	setState();
+    }
+    
+    public void prgmPressed() {
+	power++;
+	if (power > MAXPOWER)
+	    power = 0;
+	setPower();
+    }
+    
+    public void shutdown() {
+	motor.stop();
+    }
 }
--- examples/test/view/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-CLASSPATH=.
-export CLASSPATH
-../../bin/lejosc *.java
-../../bin/lejos -verbose View -o View.bin > View.sig
-
-echo "Run 'lejosrun View.bin' to download the program."
