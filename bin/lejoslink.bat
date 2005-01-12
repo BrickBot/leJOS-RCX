c30c38446074faf2efe465f58de555a69a79697c
markus_heiden <markus_heiden@4432cd44-f382-4be9-9dff-e810c426dc2b>
2005-01-12 22:31:53 +0000

    mh: fixed

--- bin/lejoslink.bat
+++ bin/lejoslink.bat
@@ -1,14 +1,10 @@
 @echo off
-set CLASSPATH_SAVE=%CLASSPATH%
 if "%LEJOS_HOME%" == ""  goto homeless
 
-set CLASSPATH=%LEJOS_HOME%\lib\jtools.jar;%CLASSPATH%
-java -Dtinyvm.linker=lejoslink -Dtinyvm.home=%LEJOS_HOME% -Dtinyvm.write.order=BE js.tinyvm.TinyVM -classpath %LEJOS_HOME%\lib\classes.jar;%LEJOS_HOME%\lib\rcxrcxcomm.jar;%CLASSPATH% %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Dtinyvm.write.order=BE -classpath %LEJOS_HOME%\lib\classes.jar;%LEJOS_HOME%\lib\rcxcomm.jar;%LEJOS_HOME%\lib\commons-cli-1.0.jar;%LEJOS_HOME%\lib\jtools.jar;%CLASSPATH% js.tinyvm.TinyVM %1 %2 %3 %4 %5 %6 %7 %8 %9 
 goto end
 
 :homeless
 echo LEJOS_HOME not defined
 
 :end
-set CLASSPATH=%CLASSPATH_SAVE%
-
