2e44a91b0d07b7e16af1199df90f5fe1f2e34fde
markus_heiden <markus_heiden@4432cd44-f382-4be9-9dff-e810c426dc2b>
2005-02-01 01:35:11 +0000

    mh: added missing classpath entry

--- trunk/lejos/bin/lejoslink.bat
+++ trunk/lejos/bin/lejoslink.bat
@@ -1,7 +1,7 @@
 @echo off
 if "%LEJOS_HOME%" == ""  goto homeless
 
-java -Dtinyvm.write.order=BE -classpath %LEJOS_HOME%\lib\classes.jar;%LEJOS_HOME%\lib\rcxcomm.jar;%LEJOS_HOME%\lib\commons-cli-1.0.jar;%LEJOS_HOME%\lib\jtools.jar;%CLASSPATH% js.tinyvm.TinyVM %1 %2 %3 %4 %5 %6 %7 %8 %9 
+java -classpath %LEJOS_HOME%\lib\classes.jar;%LEJOS_HOME%\lib\rcxcomm.jar;%LEJOS_HOME%\lib\commons-cli-1.0.jar;%LEJOS_HOME%\lib\bcel-5.1.jar;%LEJOS_HOME%\lib\jtools.jar;%CLASSPATH% js.tinyvm.TinyVM --writeorder BE %1 %2 %3 %4 %5 %6 %7 %8 %9 
 goto end
 
 :homeless
