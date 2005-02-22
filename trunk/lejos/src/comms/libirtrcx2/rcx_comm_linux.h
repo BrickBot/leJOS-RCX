2b05bd00281de7bfff8622fe5a0b39f4410957a8
markus_heiden <markus_heiden@4432cd44-f382-4be9-9dff-e810c426dc2b>
2005-02-22 14:17:07 +0000

    mh: added new version of comm libs

---
+++ trunk/lejos/src/comms/libirtrcx2/rcx_comm_linux.h
@@ -0,0 +1,55 @@
+/*
+ *  rcx_comm_linux.h
+ *
+ *  Platform specific linux RCX communication routines.
+ *
+ *  The contents of this file are subject to the Mozilla Public License
+ *  Version 1.0 (the "License"); you may not use this file except in
+ *  compliance with the License. You may obtain a copy of the License at
+ *  http://www.mozilla.org/MPL/
+ *
+ *  Software distributed under the License is distributed on an "AS IS"
+ *  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing rights and limitations
+ *  under the License.
+ *
+ *  The Original Code is Firmdl code, released October 3, 1998.
+ *
+ *  The Initial Developer of the Original Code is Kekoa Proudfoot.
+ *  Portions created by Kekoa Proudfoot are Copyright (C) 1998, 1999
+ *  Kekoa Proudfoot. All Rights Reserved.
+ *
+ *  Contributor(s): Kekoa Proudfoot <kekoa@graphics.stanford.edu>
+ */
+
+#ifndef RCX_COMM_LINUX_H_INCLUDED
+#define RCX_COMM_LINUX_H_INCLUDED
+
+#if defined(LINUX) || defined(linux)
+#define TOWER_NAME "/dev/usb/legousbtower0"
+#define DEFAULTTTY   "usb"       /* Linux - USB */
+/*
+ * Programming note (aB.)
+ * Shouldn't we use strcmp() which is a POSIX standard API (and works on Linux, Mac and any other Unix),
+ * and special-case Windows ?
+ */
+#define stricmp(x, y) strcmp(x, y)
+#define strnicmp(x, y, n) strncmp(x, y, n)
+
+#elif defined (sun)
+#define DEFAULTTTY   "/dev/ttya"  /* Solaris - first serial port - untested */
+
+#else
+#define DEFAULTTTY   "/dev/ttyd2" /* IRIX - second serial port */
+#endif
+
+#define FILEDESCR	int
+#define BADFILE	-1
+
+extern int       __rcx_write(FILEDESCR fd, const void *buf, size_t len);
+extern int       __rcx_read(FILEDESCR fd, void *buf, int maxlen, int timeout);
+extern FILEDESCR __rcx_init (char *tty, int is_fast);
+extern void      __rcx_close (FILEDESCR fd);
+
+#endif /* RCX_COMM_LINUX_H_INCLUDED */
+
---
+++ trunk/lejos/src/comms/libjirtrcx2/Tower.cpp
@@ -0,0 +1,309 @@
+/*
+*  09/23/2002 david <david@csse.uwa.edu.au> modified to support linux usb tower
+*/
+
+#include <jni.h>
+#include "josx_rcxcomm_Tower.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "rcx_comm.h"
+#include "Tower.h"
+
+#define WAKEUP_TIME_OUT 4000
+
+#if defined(_WIN32)
+#include <windows.h>
+#else
+#include <errno.h>
+
+int GetLastError() 
+{
+  return errno;
+}
+#endif
+
+//
+// Constants
+//
+
+static jfieldID _fieldIDPort;
+static jfieldID _fieldIDError;
+
+//
+// public interface
+//
+
+// open - Open the IR Tower
+JNIEXPORT jint JNICALL 
+Java_josx_rcxcomm_Tower_open(JNIEnv *env, jobject obj, jstring jport, jboolean fastMode)
+{
+  int result = 0;
+
+#ifdef TRACE
+  rcxSetDebug(true);
+  printf("Entering open\n");
+#endif
+
+   // Get the port parameter 
+  char* tty = (char*) env->GetStringUTFChars(jport, 0);
+
+  // Get a handle for the tower device
+  void* port = rcxOpen(tty, fastMode);
+  if (port == NULL) 
+  {
+	  result = RCX_OPEN_FAIL;
+  } 
+  else if (!rcxIsUsb(port)) 
+  {
+    // Only serial tower needs wake-up
+	  result = rcxWakeupTower(port, WAKEUP_TIME_OUT);
+  }
+
+  setError(env, obj, result != 0);
+  setPort(env, obj, port);
+
+  env->ReleaseStringUTFChars(jport, tty);
+
+#ifdef TRACE
+  printf("Exiting open\n");
+#endif
+  return (jint) result;
+}
+
+// close - Close the IR Tower
+JNIEXPORT jint JNICALL 
+Java_josx_rcxcomm_Tower_close(JNIEnv *env, jobject obj)
+{
+#ifdef TRACE
+  printf("Entering close\n");
+#endif
+
+  // Close the handle
+  void* port = getPort(env, obj);
+  if (port == NULL) 
+  {
+#ifdef TRACE
+      printf("File already closed\n");
+#endif
+      return (jint) RCX_ALREADY_CLOSED;
+  }
+  
+  rcxClose(port);
+  setPort(env, obj, NULL);
+
+#ifdef TRACE
+  printf("Exiting close\n");
+#endif
+
+  return RCX_OK;
+}
+
+// write - write bytes to IR Tower
+JNIEXPORT jint JNICALL
+Java_josx_rcxcomm_Tower_write(JNIEnv *env, jobject obj, jbyteArray arr, jint n)
+{
+#ifdef TRACE
+    printf("Entering write\n");
+#endif
+
+    // Check that file is open
+    void* port = getPort(env, obj);
+    if (port == NULL) 
+    {
+#ifdef TRACE
+        printf("File not open\n");
+#endif
+        return (jint) RCX_NOT_OPEN;
+    }
+
+    jbyte* body = env->GetByteArrayElements(arr, 0);
+    int result = rcxWrite(port, body, n);
+    setError(env, obj, result < 0);
+    env->ReleaseByteArrayElements(arr, body, 0);
+
+#ifdef TRACE
+    printf("Exiting write\n");
+#endif
+    return (jint) result;
+}
+
+// read - Read Bytes from IR Tower
+JNIEXPORT jint JNICALL
+Java_josx_rcxcomm_Tower_read(JNIEnv *env, jobject obj, jbyteArray arr, jint timeout)
+{
+#ifdef TRACE
+    printf("Entering read\n");
+#endif
+
+    // Check that file is open
+    void* port = getPort(env, obj);
+    if (port == NULL) 
+    {
+#ifdef TRACE
+        printf("File not open\n");
+#endif
+        return (jint) RCX_NOT_OPEN;
+    }
+
+    int size = env->GetArrayLength(arr);
+    jbyte* body = env->GetByteArrayElements(arr, 0);
+    int result = rcxRead(port, body, size, timeout);
+    env->ReleaseByteArrayElements(arr, body, 1);
+
+	 setError(env, obj, result < 0);
+
+#ifdef TRACE
+    printf("Exiting read\n");
+#endif  
+    return (jint) result;
+}
+
+// send - send a message to IR Tower
+JNIEXPORT jint JNICALL
+Java_josx_rcxcomm_Tower_send(JNIEnv *env, jobject obj, jbyteArray arr, jint n)
+{
+#ifdef TRACE
+    printf("Entering send\n");
+#endif
+
+    // Check that file is open
+    void* port = getPort(env, obj);
+    if (port == NULL) 
+    {
+#ifdef TRACE
+        printf("File not open\n");
+#endif
+
+        return (jint) RCX_NOT_OPEN;
+    }
+
+    jbyte* body = env->GetByteArrayElements(arr, 0);
+    int result = rcxSend(port, body, n);
+    env->ReleaseByteArrayElements(arr, body, 0);
+
+    setError(env, obj, result < 0);
+
+#ifdef TRACE
+    printf("Exiting send\n");
+#endif
+    return (jint) result;
+}
+
+// read - Read Bytes from IR Tower
+JNIEXPORT jint JNICALL
+Java_josx_rcxcomm_Tower_receive(JNIEnv *env, jobject obj, jbyteArray arr, jint timeout)
+{
+#ifdef TRACE
+    printf("Entering receive\n");
+#endif
+
+    // Check that file is open
+    void* port = getPort(env, obj);
+    if (port == NULL) 
+    {
+#ifdef TRACE
+        printf("File not open\n");
+#endif
+
+        return (jint) RCX_NOT_OPEN;
+    }
+
+    int size = env->GetArrayLength(arr);
+    jbyte* body = env->GetByteArrayElements(arr, NULL);
+    int actual = rcxReceive(port, body, size, timeout);
+    env->ReleaseByteArrayElements(arr, body, 1);
+
+    setError(env, obj, actual < 0);
+
+#ifdef TRACE
+    printf("Exiting receive\n");
+#endif  
+    return (jint) actual;
+}
+
+// isRCXAlive - test if RCX is alive
+JNIEXPORT jboolean JNICALL
+Java_josx_rcxcomm_Tower_isRCXAlive(JNIEnv *env, jobject obj)
+{
+#ifdef TRACE
+    printf("Entering isRCXAlive\n");
+#endif
+  
+    // Check if RCX is alive
+    void* port = getPort(env, obj);
+    bool result = rcxIsAlive(port) == 1;
+
+#ifdef TRACE
+    printf("Exiting isRCXAlive\n");
+#endif
+    return result? JNI_TRUE : JNI_FALSE;
+}
+
+// isUSB - test if IR Tower is an usb tower
+JNIEXPORT jboolean JNICALL
+Java_josx_rcxcomm_Tower_isUSB(JNIEnv *env, jobject obj)
+{
+#ifdef TRACE
+    printf("Entering isUSB\n");
+#endif
+  
+    // Check if RCX is alive
+    void* port = getPort(env, obj);
+    bool result = rcxIsUsb(port);
+
+#ifdef TRACE
+    printf("Exiting isUSB\n");
+#endif
+    return result? JNI_TRUE : JNI_FALSE;
+}
+
+//
+// Java attribute interface
+//
+
+void setError (JNIEnv* env, jobject obj, bool error)
+{
+   env->SetIntField(obj, _fieldIDError, error? GetLastError() : 0);
+}
+
+void setPort (JNIEnv* env, jobject obj, void* port)
+{
+   // the warning regarding this cast may be ignored
+   env->SetLongField(obj, _fieldIDPort, (jlong) port);
+}
+
+void* getPort (JNIEnv* env, jobject obj)
+{
+   // the warning regarding this cast may be ignored
+   return (void*) env->GetLongField(obj, _fieldIDPort);
+}
+
+// init - Init class
+JNIEXPORT void JNICALL 
+Java_josx_rcxcomm_Tower_init(JNIEnv *env, jclass clazz)
+{
+#ifdef TRACE
+  printf("Entering init\n");
+#endif
+   _fieldIDPort = env->GetFieldID(clazz, "_port", "J");
+   if (_fieldIDPort == NULL)
+   {
+#ifdef TRACE
+      printf("Could not get port field.\n");
+#endif
+   	return;
+   }
+   _fieldIDError = env->GetFieldID(clazz, "_error", "I");
+   if (_fieldIDError == NULL)
+   {
+#ifdef TRACE
+      printf("Could not get error code field.\n");
+#endif
+   	return;
+   }
+#ifdef TRACE
+  printf("Exiting init\n");
+#endif
+}
