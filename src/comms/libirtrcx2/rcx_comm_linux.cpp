217c8cd3bb6f3c8960b6e2988fc61f31d33542af
markus_heiden <markus_heiden@4432cd44-f382-4be9-9dff-e810c426dc2b>
2005-02-23 18:58:04 +0000

    mh: worked

--- src/comms/libirtrcx2/rcx_comm_linux.cpp
+++ src/comms/libirtrcx2/rcx_comm_linux.cpp
@@ -33,147 +33,163 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+
 #include <unistd.h>
 #include <termios.h>
 #include <sys/time.h>
+
 #include <stdio.h>
 #include <ctype.h>
-#include <string.h>
 #include <errno.h>
+#include <string.h>
 
-#include "rcx_comm.h"
 #include "rcx_comm_linux.h"
+#include "rcx_comm.h"
+#include "rcx_comm.cpp"
 
-#include <asm/ioctl.h>
-#include "legousbtower.h"
+//
+// prototypes for some internal methods
+//
 
+// Set serial port parameters.
+// Returns if success.
+bool __rcx_open_setSerialPortParameters (Port* port);
 
-/* Defines */
-
-#define BUFFERSIZE  4096
-
-/* Globals */
-
-extern int __comm_debug;
-
-extern int usb_flag;
+//
+// implementation
+//
 
 void __rcx_perror(char * str) 
 {
  	if (__comm_debug) perror(str);
 }
 
-/* Timeout read routine */
-
-int __rcx_read (FILEDESCR fd, void *buf, int maxlen, int timeout)
+int __rcx_read (void* port, void *buf, int maxlen, int timeout)
 {
 	long len = 0;
     
-	char *bufp = (char *)buf;
+	char *bufp = (char*) buf;
 
 	int count;
+	FILEDESCR fd = ((Port*) port)->fileHandle;
 	fd_set fds;
 	struct timeval tv;
 	int retry = 10;
 
-	while (len < maxlen) {
+	while (len < maxlen) 
+	{
 		FD_ZERO(&fds);
 		FD_SET(fd, &fds);
 
 		tv.tv_sec = timeout / 1000;
 		tv.tv_usec = (timeout % 1000) * 1000;
 
-		if (select(fd+1, &fds, NULL, NULL, &tv) < 0) {
+		if (select(fd+1, &fds, NULL, NULL, &tv) < 0) 
+		{
 			perror("select");
 			return RCX_READ_FAIL;
 		}
-		if (!FD_ISSET(fd, &fds)) {
-			if (len > 0 || retry == 0) {
+		if (!FD_ISSET(fd, &fds)) 
+		{
+			if (len > 0 || retry == 0) 
+			{
 				break;
-			} else {
+			} 
+			else 
+			{
 				retry--;
 			}
 		}	
-		if ((count = read(fd, &bufp[len], maxlen - len)) < 0) {
+		
+		if ((count = read(fd, &bufp[len], maxlen - len)) < 0) 
+		{
 			perror("read");
 			return RCX_READ_FAIL;
 		}
+		
 		len += count;
 	}
+	
 	return len;
 }
 
-/* discard all characters in the input queue of tty */
-void __rcx_flush(FILEDESCR fd)
+int __rcx_write(void* port, const void *buf, int len) 
 {
+	return write(((Port*) port)->fileHandle, buf, len);
+}
+
+void __rcx_purge(void* port)
+{
+	// TODO purge
+}
+
+void __rcx_flush(void* port)
+{
+	// TODO check
 	char echo[BUFFERSIZE];
-	__rcx_read(fd, echo, BUFFERSIZE, 200);
+	__rcx_read(((Port*) port)->fileHandle, echo, BUFFERSIZE, 200);
 }
 
-int __rcx_write(FILEDESCR fd, const void *buf, size_t len) 
+void* __rcx_open(char *tty, int is_fast)
 {
-
-	return write(fd, buf, len);
-
-}
-
-/* RCX routines */
-
-FILEDESCR __rcx_init(char *tty, int is_fast)
-{
-	FILEDESCR fd;
-
-	struct termios ios;
-
 	if (__comm_debug) printf("mode = %s\n", is_fast ? "fast" : "slow");
 	if (__comm_debug) printf("tty= %s\n", tty);
 
+	bool success = true;
 
-	if ((fd = open(tty, O_RDWR)) < 0) { 
-		perror(tty);
-		exit(1);
+	Port* result = (Port*) malloc(sizeof(Port));
+	
+	result->fileHandle = open(tty, O_RDWR);
+	if (result->fileHandle < 0) 
+	{ 
+		if (__comm_debug) printf("Error %lu: Opening %s\n", errno, result->deviceName);
+		success = false;
+	}
+	else if (!result->usb)
+	{
+      // Setup serial port
+      success = __rcx_open_setSerialPortParameters(result);
+		if (__comm_debug && !success) printf("Error %lu: Setting serial port parameters for %s\n", errno, result->deviceName);
 	}
 
-	/* Assume USB for all non-tty devices */
-	usb_flag = !isatty(fd);
-	if (__comm_debug) printf("usb_flag = %d\n", usb_flag);
-
-	if (usb_flag) {
-		if (is_fast) {
-			fprintf(stderr, "FAST mode not allowed with USB LINUX\n");
-			return RCX_OPEN_FAIL;
-		} 
-	} else {
-
-		if (!isatty(fd)) {
-			close(fd);
-			fprintf(stderr, "%s: not a tty\n", tty);
-			return BADFILE;
+	if (!success)
+	{
+		if (result->fileHandle > 0)
+		{
+			close(result->fileHandle);
 		}
-
-		memset(&ios, 0, sizeof(ios));
-	    
-		if (is_fast) {
-			ios.c_cflag = CREAD | CLOCAL | CS8;
-			cfsetispeed(&ios, B4800);
-			cfsetospeed(&ios, B4800);
-		}
-		else {
-			ios.c_cflag = CREAD | CLOCAL | CS8 | PARENB | PARODD;
-			cfsetispeed(&ios, B2400);
-			cfsetospeed(&ios, B2400);
-		}
-	    
-		if (tcsetattr(fd, TCSANOW, &ios) == -1) {
-			perror("tcsetattr");
-			return BADFILE;
-		}
+		free(result);
 	}
 
-	return fd;
+	if (__comm_debug) printf("device = %s\n", result->deviceName);
+	if (__comm_debug) printf("port type = %s\n", result->usb? "usb" : "serial");
+
+	return result;
 }
 
-void __rcx_close(FILEDESCR fd)
+bool __rcx_open_setSerialPortParameters (Port* port)
 {
-	close(fd);
+	struct termios ios;
+	memset(&ios, 0, sizeof(ios));
+    
+	if (port->fast) 
+	{
+		ios.c_cflag = CREAD | CLOCAL | CS8;
+		cfsetispeed(&ios, B4800);
+		cfsetospeed(&ios, B4800);
+	}
+	else 
+	{
+		ios.c_cflag = CREAD | CLOCAL | CS8 | PARENB | PARODD;
+		cfsetispeed(&ios, B2400);
+		cfsetospeed(&ios, B2400);
+	}
+    
+	return tcsetattr(port->fileHandle, TCSANOW, &ios) != -1;
+}
+
+void __rcx_close(void* port)
+{
+	close(((Port*) port)->fileHandle);
+	free(port);
 }
