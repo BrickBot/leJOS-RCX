f7873dcaf056fff13c7a1ec652f42512ed35aa66
mpscholz <mpscholz@4432cd44-f382-4be9-9dff-e810c426dc2b>
2005-03-07 17:12:19 +0000

    run_works_now

--- test/regression/run1.sh
+++ test/regression/run1.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-set -x
 export CLASSPATH=.
 export PATH=.:../bin:$PATH
 
 echo ------------------ Compiling $1
-lejosc $1.java
+lejosjc $1.java
 echo ------------------ Linking $1
-emu-lejos -verbose -o $1.tvm $1 >$1.sig
+emu-lejos -o $1.tvm $1 >$1.sig
 echo ------------------ Running $1
 emu-lejosrun -v $1.tvm
