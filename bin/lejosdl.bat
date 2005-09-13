2a65240bffc46b42daeae621f779d259e8a8faa4
markus_heiden <markus_heiden@4432cd44-f382-4be9-9dff-e810c426dc2b>
2005-09-13 16:56:09 +0000

    mh: reworked startup scripts, removed ld_preload

--- bin/lejosdl.bat
+++ bin/lejosdl.bat
@@ -1,5 +1,10 @@
 @echo off
-if "$LEJOS_HOME" == "" goto end
-java -classpath %LEJOS_HOME%\lib\jtools.jar;%LEJOS_HOME%\lib\commons-cli-1.0.jar;%LEJOS_HOME%\lib\pcrcxcomm.jar js.tools.Lejosdl %1 %2 %3 %4 %5 %6 %7 %8 %9
-:end
+if "%LEJOS_HOME%" == ""  goto homeless
 
+java -classpath %LEJOS_HOME%\lib\jtools.jar;%LEJOS_HOME%\lib\commons-cli-1.0.jar;%LEJOS_HOME%\lib\pcrcxcomm.jar js.tools.Lejosdl %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto end
+
+:homeless
+echo LEJOS_HOME not defined
+
+:end
