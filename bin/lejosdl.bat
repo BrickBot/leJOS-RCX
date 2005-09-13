05731fed030c9958959ee7adbcdd77f5888f88ed
markus_heiden <markus_heiden@4432cd44-f382-4be9-9dff-e810c426dc2b>
2005-09-13 18:02:17 +0000

    mh: added native library path

--- bin/lejosdl.bat
+++ bin/lejosdl.bat
@@ -1,7 +1,7 @@
 @echo off
 if "%LEJOS_HOME%" == ""  goto homeless
 
-java -classpath %LEJOS_HOME%\lib\jtools.jar;%LEJOS_HOME%\lib\commons-cli-1.0.jar;%LEJOS_HOME%\lib\pcrcxcomm.jar js.tools.Lejosdl %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Djava.library.path="%LEJOS_HOME%\bin" -classpath "%LEJOS_HOME%\lib\jtools.jar;%LEJOS_HOME%\lib\commons-cli-1.0.jar;%LEJOS_HOME%\lib\pcrcxcomm.jar" js.tools.Lejosdl %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto end
 
 :homeless
