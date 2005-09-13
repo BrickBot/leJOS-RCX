ef013a6856c14d8c038c69e981456ce3c1958413
markus_heiden <markus_heiden@4432cd44-f382-4be9-9dff-e810c426dc2b>
2005-09-13 16:56:09 +0000

    mh: reworked startup scripts, removed ld_preload

--- trunk/lejos/bin/lejosjc.bat
+++ trunk/lejos/bin/lejosjc.bat
@@ -1,5 +1,10 @@
 @echo off
-if "$LEJOS_HOME" == "" goto end
-javac -target 1.1 -bootclasspath %LEJOS_HOME%\lib\classes.jar;%LEJOS_HOME%\lib\rcxrcxcomm.jar;%CLASSPATH% %1 %2 %3 %4 %5 %6 %7 %8 %9
-:end
+if "%LEJOS_HOME%" == ""  goto homeless
 
+javac -target 1.1 -bootclasspath %LEJOS_HOME%\lib\classes.jar;%LEJOS_HOME%\lib\rcxrcxcomm.jar;%CLASSPATH% %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto end
+
+:homeless
+echo LEJOS_HOME not defined
+
+:end
