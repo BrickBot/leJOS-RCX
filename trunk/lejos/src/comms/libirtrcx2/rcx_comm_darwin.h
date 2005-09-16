3e0326660bb5a54f1d7ac5063252bafa43ef7c4f
markus_heiden <markus_heiden@4432cd44-f382-4be9-9dff-e810c426dc2b>
2005-09-16 13:06:43 +0000

    mh: updated mac code

---
+++ trunk/lejos/src/comms/libirtrcx2/rcx_comm_darwin.h
@@ -0,0 +1,40 @@
+/*
+ *  rcx_comm_osx.h
+ *
+ *  Platform specific linux RCX communication routines.
+ *
+ *  The contents of this file are subject to the Mozilla Public License
+ *  Version 1.0 (the "License"); you may not use this file except in
+ *  compliance with the License. You may obtain a copy of the License at
+ *  http://www.mozilla.org/MPL/
+ *
+ *  Software distributed under the License is distributed on an "AS IS"
+ *  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing rights and limitations
+ *  under the License.
+ *
+ *  The Original Code is Firmdl code, released October 3, 1998.
+ *
+ *  The Initial Developer of the Original Code is Kekoa Proudfoot.
+ *  Portions created by Kekoa Proudfoot are Copyright (C) 1998, 1999
+ *  Kekoa Proudfoot. All Rights Reserved.
+ *
+ *  Contributor(s): Kekoa Proudfoot <kekoa@graphics.stanford.edu>
+ */
+
+#ifndef RCX_COMM_OSX_H_INCLUDED
+#define RCX_COMM_OSX_H_INCLUDED
+
+#define TOWER_NAME ""
+#define DEFAULTTTY   "usb"	  /* Default to USB on MAC */
+#define stricmp(x, y) strcmp(x, y)
+#define strnicmp(x, y, n) strncmp(x, y, n)
+
+/* Support storage of file descriptors or IODevices in a FILEDESCR */
+#include <stdint.h>
+#define FILEDESCR	intptr_t
+#define BADFILE	-1
+/* O_SYNC not defined on Mac OS X */
+#define O_SYNC 0
+
+#endif /* RCX_COMM_OSX_H_INCLUDED */
