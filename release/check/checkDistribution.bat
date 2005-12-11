c5da14cb64200483df34b3ff707480513a271050
mpscholz <mpscholz@4432cd44-f382-4be9-9dff-e810c426dc2b>
2005-12-11 18:44:41 +0000

    M1

--- release/check/checkDistribution.bat
+++ release/check/checkDistribution.bat
@@ -5,21 +5,29 @@
 #
 # 02/12/05  created Matthias Paul Scholz
 
-if "%LEJOS_HOME%" != "" goto lejos_home_set
+echo checking environment
+cd ..
+
+if "%LEJOS_HOME%" != "%PWD%" goto lejos_home_set
 
 :lejos_home_not_set  
-	REM ERROR: environment variable LEJOS_HOME is not set!
-	REM Please export it, pointing to the directory you installed this distribution into.
+	REM ERROR: environment variable LEJOS_HOME is pointing to %LEJOS_HOME% presently
+  	REM however, it has to point to %PWD%. 
+  	REM please set it accordingly.
+  	cd .\check
 	goto end
 
 :lejos_home_set  
 
+    REM environment seems to be ok
+    cd .\check
 	REM compiling test class
 	..\bin\lejosjc DistributionSmokeTest.java
 	REM linking test class
 	..\bin\lejoslink DistributionSmokeTest -o DistributionSmokeTest.bin
 	REM downloading test class
 	..\bin\lejosdl DistributionSmokeTest.bin
-	REM Done. Now press the RUN Button on your RCX...
+	REM done. 
+	REM if no error occurred, please press the RUN Button on your RCX now...
 
 : end
