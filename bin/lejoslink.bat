08fdb4fd1ed8b32e446be608cf8705d0d1695619
mpscholz <mpscholz@4432cd44-f382-4be9-9dff-e810c426dc2b>
2006-02-03 17:08:21 +0000

    lejos3.0_M2

--- bin/lejoslink.bat
+++ bin/lejoslink.bat
@@ -1,10 +1,10 @@
-@echo off
+echo off
 if "%LEJOS_HOME%" == ""  goto homeless
 
-THIRDPARTY_LIBS="%LEJOS_HOME%\3rdparty\lib"
-LINK_CLASSPATH=.;%THIRDPARTY_LIBS%\bcel-5.1.jar;%THIRDPARTY_LIBS%\commons-cli-1.0.jar;%LEJOS_HOME%\lib\jtools.jar;%LEJOS_HOME%\lib\classes.jar;%LEJOS_HOME%\lib\rcxcomm.jar
+set THIRDPARTY_LIBS="%LEJOS_HOME%\3rdparty\lib"
+set LINK_CLASSPATH=.;%THIRDPARTY_LIBS%\bcel-5.1.jar;%THIRDPARTY_LIBS%\commons-cli-1.0.jar;%LEJOS_HOME%\lib\jtools.jar;%LEJOS_HOME%\lib\classes.jar;%LEJOS_HOME%\lib\rcxcomm.jar
 
-java -classpath %LINK_CLASSPATH% js.tinyvm.TinyVM --writeorder BE %1 %2 %3 %4 %5 %6 %7 %8 %9 
+java -classpath "%LINK_CLASSPATH%" js.tinyvm.TinyVM --writeorder BE --classpath "%LINK_CLASSPATH%" %1 %2 %3 %4 %5 %6 %7 %8 %9 
 goto end
 
 :homeless
--- release/check/checkDistribution.bat
+++ release/check/checkDistribution.bat
@@ -1,33 +1,34 @@
-#!/bin/bash
+@echo off
 
-#
-# checkDistribution: utility to check the lejOS distribution after having downloaded it
-#
-# 02/12/05  created Matthias Paul Scholz
+rem
+rem checkDistribution: utility to check the lejOS distribution after having downloaded it
+rem
+rem 02/12/05  created Matthias Paul Scholz
+
 
 echo checking environment
 cd ..
 
-if "%LEJOS_HOME%" != "%PWD%" goto lejos_home_set
+if not "%LEJOS_HOME%" == "%PWD%" goto lejos_home_set
 
 :lejos_home_not_set  
-	REM ERROR: environment variable LEJOS_HOME is pointing to %LEJOS_HOME% presently
-  	REM however, it has to point to %PWD%. 
-  	REM please set it accordingly.
+	echo ERROR: environment variable LEJOS_HOME is pointing to %LEJOS_HOME% presently
+  	echo however, it has to point to %PWD%. 
+  	echo please set it accordingly.
   	cd .\check
 	goto end
 
 :lejos_home_set  
 
-    REM environment seems to be ok
+    echo environment seems to be ok
     cd .\check
-	REM compiling test class
-	..\bin\lejosjc DistributionSmokeTest.java
-	REM linking test class
-	..\bin\lejoslink DistributionSmokeTest -o DistributionSmokeTest.bin
-	REM downloading test class
-	..\bin\lejosdl DistributionSmokeTest.bin
-	REM done. 
-	REM if no error occurred, please press the RUN Button on your RCX now...
+	echo compiling test class
+	..\bin\lejosjc.bat DistributionSmokeTest.java
+	echo linking test class
+	..\bin\lejoslink.bat DistributionSmokeTest -o DistributionSmokeTest.bin
+	echo downloading test class
+	..\bin\lejosdl.bat DistributionSmokeTest.bin
+	echo done. 
+	echo if no error occurred, please press the RUN Button on your RCX now...
 
 : end
