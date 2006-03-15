4ddcd4c8e77a98bdf812046b4bef96b091d07817
mpscholz <mpscholz@4432cd44-f382-4be9-9dff-e810c426dc2b>
2006-03-15 14:04:45 +0000

    lejos 3.0.0 releated adjustments

--- trunk/lejos/bin/lejosjc.bat
+++ trunk/lejos/bin/lejosjc.bat
@@ -1,7 +1,8 @@
 @echo off
 if "%LEJOS_HOME%" == ""  goto homeless
 
-javac -target 1.1 -bootclasspath %LEJOS_HOME%\lib\classes.jar;%LEJOS_HOME%\lib\rcxrcxcomm.jar;%CLASSPATH% %1 %2 %3 %4 %5 %6 %7 %8 %9
+javac -bootclasspath %LEJOS_HOME%\lib\classes.jar;%LEJOS_HOME%\lib\rcxrcxcomm.jar;%CLASSPATH% %1 %2 %3 %4 %5 %6 %7 %8 %9
+rem javac -target 1.1 -bootclasspath %LEJOS_HOME%\lib\classes.jar;%LEJOS_HOME%\lib\rcxrcxcomm.jar;%CLASSPATH% %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto end
 
 :homeless
